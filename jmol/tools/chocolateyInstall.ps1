04032c260a2cab1acafdc35c0580f2c3b65248c5
teknowledgist <teknowledgist@gmail.com>
2016-04-22 13:00:42 -0400

    Install script for jmol

---
+++ jmol/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'jmol'
+$url = 'http://sourceforge.net/projects/jmol/files/latest/download'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Path)
+
+Install-ChocolateyZipPackage $packageName $url $scriptPath
+
+$JavaExe = 'C:\ProgramData\Oracle\Java\javapath\javaw.exe'
+$target = (Get-ChildItem $scriptPath -filter jmol.jar -Recurse).fullname
+$icon = Join-Path $scriptPath "Jmol_icon13.ico"
+$launcher = Join-Path $scriptPath 'Jmol Launcher.exe'
+$ShimGen = Join-Path "$helpersPath" '..\tools\shimgen.exe'
+
+& $ShimGen -o $launcher -p $JavaExe -c "-jar '$target'" -i $icon --gui
+
+$shortcut = Join-Path ([Environment]::GetFolderPath("Desktop")) 'Jmol.lnk'
+
+Install-ChocolateyShortcut -ShortcutFilePath $shortcut -TargetPath $launcher -IconLocation $icon
