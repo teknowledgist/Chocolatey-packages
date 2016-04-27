9e5ecb2879c19b80aea30537808a9b365befdea8
teknowledgist <teknowledgist@gmail.com>
2016-04-27 09:01:06 -0400

    Added files via upload

---
+++ deepview/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$PackageDir = Split-path (Split-path $MyInvocation.MyCommand.Definition)
+
+$InstallArgs = @{
+   PackageName = 'deepview'
+   Url = 'http://spdbv.vital-it.ch/download/binaries/SPDBV_4.10_PC.zip' 
+   UnzipLocation = Split-path (Split-path $MyInvocation.MyCommand.Definition)
+}
+Install-ChocolateyZipPackage @InstallArgs
+
+$target   = Join-Path $InstallArgs.UnzipLocation 'SPDBV_4.10_PC\spdbv.exe'
+$shortcut = Join-Path ([System.Environment]::GetFolderPath('Desktop')) 'Swiss-PdbViewer.lnk'
+
+Install-ChocolateyShortcut -ShortcutFilePath $shortcut -TargetPath $target
+
---
+++ deepview/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$desktop = $([Environment]::GetFolderPath([Environment+SpecialFolder]::DesktopDirectory))
+$shortcut = Join-Path $desktop 'Swiss-PdbViewer.lnk'
+ 
+if ([System.IO.File]::Exists($shortcut)) {
+    Write-Debug "Found the desktop shortcut. Deleting it..."
+    [System.IO.File]::Delete($shortcut)
+}
