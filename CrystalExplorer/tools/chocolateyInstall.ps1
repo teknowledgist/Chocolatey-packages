54f2356325de2aa649f3dded0868b65c41a52ce0
teknowledgist <teknowledgist@users.noreply.github.com>
2016-05-05 09:50:13 -0400

    upload crystalexplorer

---
+++ CrystalExplorer/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$InstallArgs = @{
+   PackageName = 'crystalexplorer'
+   Url = 'http://crystalexplorer.scb.uwa.edu.au/downloads/CrystalExplorer3.1_Windows-Intel-32bit.zip' 
+   UnzipLocation = ${env:ProgramFiles(x86)}
+}
+Install-ChocolateyZipPackage @InstallArgs
+
+$shortcut = Join-Path ([Environment]::GetFolderPath('CommonDesktopDirectory')) 'CrystalExplorer.lnk'
+$target = (Get-ChildItem (join-path ${env:ProgramFiles(x86)} 'CrystalExplorer*') -include cry*.exe -Recurse).fullname
+
+Install-ChocolateyShortcut -ShortcutFilePath $shortcut -TargetPath $target
---
+++ CrystalExplorer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿if (test-path (Join-Path $env:ProgramFiles 'CrystalExplorer3.1')) {
+   Remove-Item (Join-Path $env:ProgramFiles 'CrystalExplorer3.1') -Recurse -Force
+}
+
+$shortcut = Join-Path ([Environment]::GetFolderPath('CommonDesktopDirectory')) 'CrystalExplorer.lnk'
+
+if (Test-Path $shortcut) {
+   Remove-Item $shortcut
+}
