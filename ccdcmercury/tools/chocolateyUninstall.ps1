6eae7789fed7fa107122e16d4ad01f6bb77bf892
teknowledgist <teknowledgist@users.noreply.github.com>
2016-05-05 11:39:24 -0400

    upload mercury

---
+++ ccdcmercury/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+﻿$UninstallArgs = @{
+   packageName = 'ccdcmercury'
+   fileType = 'exe'
+   file = "C:\Program Files (x86)\CCDC\Mercury 3.8\uninstall.exe"
+   silentArgs = '--mode unattended'
+   validExitCodes = @(0)
+}
+
+Uninstall-ChocolateyPackage @UninstallArgs
+
+
+
+
+
