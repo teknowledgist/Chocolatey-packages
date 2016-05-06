b120466c014e6b39ebbf182f5f1e9beeeaf89b19
teknowledgist <teknowledgist@users.noreply.github.com>
2016-05-06 08:52:59 -0400

    changes to mercury package

--- ccdcmercury/tools/chocolateyUninstall.ps1
+++ ccdcmercury/tools/chocolateyUninstall.ps1
@@ -1,7 +1,20 @@
-﻿$UninstallArgs = @{
+﻿$RegistryLocation = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+
+# different key for 32-bit installs on 64-bit systems
+$BitLevel = Get-ProcessorBits
+If ($BitLevel -eq '64') {
+   $RegistryLocation = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+}
+
+$unexe = Get-ItemProperty "$RegistryLocation\*" | 
+                     Where-Object { $_.displayname -match 'Mercury'} |
+                     Select-Object -ExpandProperty UninstallString
+
+
+$UninstallArgs = @{
    packageName = 'ccdcmercury'
    fileType = 'exe'
-   file = "C:\Program Files (x86)\CCDC\Mercury 3.8\uninstall.exe"
+   file = $unexe
    silentArgs = '--mode unattended'
    validExitCodes = @(0)
 }
