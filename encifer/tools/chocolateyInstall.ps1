31fbb996269cc718cc04c69c05967e6a90bea4c4
teknowledgist <teknowledgist@users.noreply.github.com>
2016-05-06 08:47:23 -0400

    upload encifer

---
+++ encifer/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$InstallArgs = @{
+   packageName = 'encifer'
+   installerType = 'exe'
+   url = 'https://downloads.ccdc.cam.ac.uk/enCIFer/1.5/encifer-1.5.1-windows-installer.exe'
+   silentArgs = '--mode unattended --prefix "C:\Program Files (x86)\CCDC\enCIFer 1.5.1"'
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
+
---
+++ encifer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,27 @@
+﻿$RegistryLocation = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+
+# different key for 32-bit installs on 64-bit systems
+$BitLevel = Get-ProcessorBits
+If ($BitLevel -eq '64') {
+   $RegistryLocation = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+}
+
+$unexe = Get-ItemProperty "$RegistryLocation\*" | 
+                     Where-Object { $_.displayname -match 'encifer'} |
+                     Select-Object -ExpandProperty UninstallString
+
+
+$UninstallArgs = @{
+   packageName = 'encifer'
+   fileType = 'exe'
+   file = $unexe
+   silentArgs = '--mode unattended'
+   validExitCodes = @(0)
+}
+
+Uninstall-ChocolateyPackage @UninstallArgs
+
+
+
+
+
