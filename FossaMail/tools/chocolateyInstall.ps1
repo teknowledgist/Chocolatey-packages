653d5d58e65f1a08c62fba90d5789651fe81812e
teknowledgist <erich@albany.edu>
2016-05-23 23:05:09 -0400

    upload FossaMail

---
+++ FossaMail/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$InstallArgs = @{
+  packageName = 'fossamail'
+  installerType = 'exe'
+  url = 'http://relmirror.fossamail.org/25.2.1/FossaMail-25.2.1.win32.installer.exe'
+  url64 = 'http://relmirror.fossamail.org/25.2.1/FossaMail-25.2.1.win64.installer.exe'
+  silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
