fa7dfc78cca466ee2dfe85ebd8215f2f2bc90aa7
teknowledgist <teknowledgist@users.noreply.github.com>
2016-08-04 13:09:09 -0400

    complete lastools

--- lastools/tools/chocolateyInstall.ps1
+++ lastools/tools/chocolateyInstall.ps1
@@ -3,34 +3,26 @@ $ErrorActionPreference = 'Stop'  # stop on all errors
 $ZipArgs = @{
    PackageName = 'lastools'
    Url = 'http://www.cs.unc.edu/~isenburg/lastools/download/LAStools.zip'
-   UnzipLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
+   UnzipLocation = Split-Path (Split-Path -parent $MyInvocation.MyCommand.Definition)
 }
 
 Install-ChocolateyZipPackage @ZipArgs
 
+$exes = Get-ChildItem $ZipArgs.UnzipLocation -filter *.exe -Recurse |select -ExpandProperty fullname
+$txts = Get-ChildItem $ZipArgs.UnzipLocation -filter *.txt -Recurse |select -ExpandProperty fullname
 
+foreach ($exe in $exes) {
+   if ($txts -notcontains ($exe.trim('.exe') + '_README.txt')) {
+      New-Item "$exe.ignore" -Type file -Force | Out-Null
+   }
+}
 
-  @(
-  'AccessEnum',
-  'ADExplorer',
-  'AdInsight',
-  'Autoruns',
-  'Bginfo',
-  'Cachset',
-  'Dbgview',
-  'Desktops',
-  'disk2vhd',
-  'DiskView',
-  'LoadOrd',
-  'pagedfrg',
-  'portmon',
-  'procexp',
-  'Procmon',
-  'RAMMap',
-  'RootkitRevealer',
-  'Tcpview',
-  'vmmap',
-  'ZoomIt'
-  ) | % {
-    New-Item "$installDir\$_.exe.gui" -Type file -Force | Out-Null
-  }
+$GUI = (Get-ChildItem $ZipArgs.UnzipLocation -filter lastool.exe -Recurse).fullname
+New-Item "$GUI.gui" -Type file -Force | Out-Null
+
+$ShortcutArgs = @{
+   ShortcutFilePath = Join-Path ([Environment]::GetFolderPath('Desktop')) 'LASTool.lnk'
+   TargetPath = $GUI
+}
+
+Install-ChocolateyShortcut @ShortcutArgs
--- lastools/tools/chocolateyUninstall.ps1
+++ lastools/tools/chocolateyUninstall.ps1
@@ -1,4 +1,10 @@
-$packageName = 'pydev-eclipse-java-neon'
+$packageName = 'lastools'
+
+$shortcut = Join-Path ([Environment]::GetFolderPath('Desktop')) 'LAStool.lnk'
+if (Test-Path $shortcut) {
+    Write-Verbose 'Found the desktop shortcut. Deleting it...'
+    Remove-Item $shortcut
+}
 
 # This avoids having to modify uninstall if the zip install file changes names
 $installedZipFile = (Get-ChildItem $env:chocolateyPackageFolder -filter '*.zip.txt').name.trim('.txt')
