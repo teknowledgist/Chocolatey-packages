3a2c2b4cd9864561719aee54dd68e176fe3f8565
teknowledgist <erich@albany.edu>
2016-08-04 19:54:23 -0400

    fusion fixes

---
+++ fusion-ldv/tools/chocolateyInstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'  # stop on all errors
+
+$InstallPath = Join-Path $env:ProgramData 'Fusion-LDV'
+
+$InstallArgs = @{
+   packageName = 'fusion-ldv'
+   installerType = 'exe'
+   url = 'http://forsys.cfr.washington.edu/fusion/FUSION_Install.exe'
+   silentArgs = "/S /D=$InstallPath"
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
+
+# To support compressed LiDAR data, the LASzip.dll file from 
+#   the lastools package needs to be found and copied.
+$TargetPackage = 'lastools'
+$TargetLib = "$env:ChocolateyInstall\lib\$TargetPackage"
+$TargetUnzipLog = Get-ChildItem $TargetLib -Filter '*.zip.txt'
+If ($TargetUnzipLog) {
+   $TargetInstallLocation = Split-Path (Get-Content $TargetUnzipLog.FullName | Select-Object -First 1)
+   $dll = Get-ChildItem $TargetInstallLocation -Filter 'laszip.dll' -Recurse | 
+      Where-Object {$_.FullName -match '\\laszip\\'}
+   Copy-Item $dll.fullname $InstallPath
+} else {
+   Write-Debug "Chocolatey package $TargetPackage install location not found!"
+   Write-Debug 'LASzip is not available.  Compressed LiDAR data will be inaccessible.'
+   Write-Debug 'Download from laszip.org and place LASzip.dll in Fusion-LDV directory.'
+}
+
+
---
+++ fusion-ldv/tools/chocolateyUninstall.ps1
@@ -0,0 +1,22 @@
+﻿# Find where Fusion-LDV installed itself (no registry entry!)
+$StartMenu = Join-Path $env:ProgramData '\Microsoft\Windows\Start Menu'
+$SMlink = Get-ChildItem $StartMenu -Filter 'Fusion.lnk' -Recurse
+$lnk = (new-object -com wscript.shell).createShortcut($SMlink.FullName)
+$InstallPath = Split-Path $lnk.targetPath
+
+# Find the uninstaller
+$Unexe = Get-ChildItem (Split-Path $lnk.targetPath) -Filter Uninstall.exe
+
+$UninstallArgs = @{
+   packageName = 'fusion-ldv'
+   fileType = 'exe'
+   file = $unexe.FullName
+   silentArgs = '/S'
+   validExitCodes = @(0)
+}
+
+Uninstall-ChocolateyPackage @UninstallArgs
+
+
+
+
