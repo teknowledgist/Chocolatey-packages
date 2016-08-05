6a0f18dbfcb8caf5d0073380a1b8d93b8e7848ef
teknowledgist <teknowledgist@users.noreply.github.com>
2016-08-05 09:40:03 -0400

    corrections

--- fusion-ldv/tools/chocolateyInstall.ps1
+++ fusion-ldv/tools/chocolateyInstall.ps1
@@ -1,37 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'  # stop on all errors
 
-<<<<<<< HEAD
-=======
 $InstallPath = Join-Path $env:ProgramData 'Fusion-LDV'
 
->>>>>>> origin/master
 $InstallArgs = @{
    packageName = 'fusion-ldv'
    installerType = 'exe'
    url = 'http://forsys.cfr.washington.edu/fusion/FUSION_Install.exe'
-<<<<<<< HEAD
-   silentArgs = '/S /D=c:\Program Files (x86)\Fusion-LDV'
-   validExitCodes = @(0)
-}
-
-# Allow users to override the install location in a "standard" way.
-if ($env:chocolateyInstallArguments -match '/D=' ) {
-   $InstallArgs.silentArgs = '/S'
-}
-Install-ChocolateyPackage @InstallArgs
-
-# To support compressed LiDAR data, the LASzip.dll file from 
-#   the lastools package needs to be copied.
-
-# First, find where Fusion-LDV ended up
-$StartMenu = Join-Path $env:ProgramData "\Microsoft\Windows\Start Menu"
-$SMlink = Get-ChildItem $StartMenu -Filter 'Fusion.lnk' -Recurse
-$lnk = (new-object -com wscript.shell).createShortcut($SMlink.FullName)
-$InstallPath = Split-Path $lnk.targetPath
-
-
-# Next, find it the dll
-=======
    silentArgs = "/S /D=$InstallPath"
    validExitCodes = @(0)
 }
@@ -40,7 +14,6 @@ Install-ChocolateyPackage @InstallArgs
 
 # To support compressed LiDAR data, the LASzip.dll file from 
 #   the lastools package needs to be found and copied.
->>>>>>> origin/master
 $TargetPackage = 'lastools'
 $TargetLib = "$env:ChocolateyInstall\lib\$TargetPackage"
 $TargetUnzipLog = Get-ChildItem $TargetLib -Filter '*.zip.txt'
@@ -50,13 +23,9 @@ If ($TargetUnzipLog) {
       Where-Object {$_.FullName -match '\\laszip\\'}
    Copy-Item $dll.fullname $InstallPath
 } else {
-<<<<<<< HEAD
-   throw "Chocolatey package $TargetPackage install location not found!"
-=======
    Write-Debug "Chocolatey package $TargetPackage install location not found!"
    Write-Debug 'LASzip is not available.  Compressed LiDAR data will be inaccessible.'
    Write-Debug 'Download from laszip.org and place LASzip.dll in Fusion-LDV directory.'
->>>>>>> origin/master
 }
 
 
