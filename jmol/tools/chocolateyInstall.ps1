083bda92aeb4f2e0f28fbd5d798fb7f433bbc006
teknowledgist <teknowledgist@users.noreply.github.com>
2016-08-15 13:18:25 -0400

    fix variable names

--- jmol/tools/chocolateyInstall.ps1
+++ jmol/tools/chocolateyInstall.ps1
@@ -8,18 +8,18 @@ $installArgs = @{
    url = "https://sourceforge.net/projects/jmol/files/Jmol/Version%20$majorVersion/Version%20$MinorVersion/Jmol-$($MinorVersion)_$SubVersion-binary.zip/download"
    Checksum = '51683330ae480115f8c17a07166c2264645a6fa1'
    ChecksumType = 'sha1'
-   scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Path)
+   UnzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Path)
 }
 
 Install-ChocolateyZipPackage @installArgs
 
 $JavaExe = 'C:\ProgramData\Oracle\Java\javapath\javaw.exe'
-$target = (Get-ChildItem $installArgs.scriptPath -filter jmol.jar -Recurse).fullname
-$icon = Join-Path $installArgs.scriptPath "Jmol_icon13.ico"
-$launcher = Join-Path $installArgs.scriptPath 'Jmol Launcher.exe'
-$SG = Join-Path $env:ChocolateyInstall 'tools\shimgen.exe'
+$target = (Get-ChildItem $installArgs.UnzipLocation -filter jmol.jar -Recurse).fullname
+$icon = Join-Path $installArgs.UnzipLocation "Jmol_icon13.ico"
+$launcher = Join-Path $installArgs.UnzipLocation 'Jmol Launcher.exe'
+$SGpath = Join-Path $env:ChocolateyInstall 'tools\shimgen.exe'
 
-& $SG -o $launcher -p $JavaExe -c "-jar '$target'" -i $icon --gui
+& $SGpath -o $launcher -p $JavaExe -c "-jar '$target'" -i $icon --gui
 
 $shortcut = Join-Path ([Environment]::GetFolderPath("Desktop")) 'Jmol.lnk'
 
