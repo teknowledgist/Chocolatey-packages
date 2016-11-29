ed7919311bc7a065361ceb85eb581cc3ebea3d4f
teknowledgist <teknowledgist@users.noreply.github.com>
2016-11-29 10:27:11 -0500

    New package

---
+++ manual/digimizer/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'  # stop on all errors
+
+$packageName= 'digimizer'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = 'https://www.digimizer.com/download/digimizersetup.msi'
+  checksum      = 'F32F093EC3B897E67767FD60E19CEDB52FE6783AD2B72CD3F34D7DE8E7E671A7'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs 
