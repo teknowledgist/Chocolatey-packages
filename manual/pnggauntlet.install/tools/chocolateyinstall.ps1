66475f5f7c0cdd6debb3eee42e13bf209c4bf160
teknowledgist <teknowledgist@users.noreply.github.com>
2017-01-06 16:43:49 -0500

    adding pnggauntlet

---
+++ manual/pnggauntlet.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName='PNGGauntlet'
+
+$DownloadFile = "$packageName-3.1.2.exe"
+$DestinationDir = Join-Path $env:TEMP $packageName
+
+$PackageArgs = @{
+  PackageName = $packageName
+  FileFullPath = Join-Path $DestinationDir $DownloadFile
+  Url = 'https://pnggauntlet.com/PNGGauntlet-3.1.2.exe'
+  checkSum = '1155CB66555764D497962D7857EB61D3EA13D5E6CA4C847FB0A4BBC46B802E83'
+  checkSumType = 'sha256'
+}
+
+# Download zip
+Get-ChocolateyWebFile @PackageArgs
+
+# Extract zip
+Get-ChocolateyUnzip (Join-Path $DestinationDir $DownloadFile) $DestinationDir
+
+$InstallArgs = @{
+   packageName   = $packageName
+   fileType      = 'msi'
+   silentArgs = "/qn"
+   validExitCodes= @(0)
+   File = (Join-Path $DestinationDir "Installer\$($packageName)Setup.msi")
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
+
