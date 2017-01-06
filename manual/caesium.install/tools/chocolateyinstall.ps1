0d7629cc4188d426ffdcca3f5d6b00d7bbf213fa
teknowledgist <teknowledgist@users.noreply.github.com>
2017-01-06 14:36:37 -0500

    adding caesium, fixing wxmacmolplt

---
+++ manual/caesium.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$installArgs = @{
+   packageName = 'Caesium.install'
+   installerType = 'exe'
+   url = 'https://sourceforge.net/projects/caesium/files/1.7.0/caesium-1.7.0-win.exe/download'
+   checkSum = '1C7CE7B925391730A2B7DBAB52DF7AC2F78D45580A423BC6B299F2E74A4B1C59'
+   checkSumType = 'sha256'
+   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @installArgs
--- manual/wxmacmolplt/tools/chocolateyinstall.ps1
+++ manual/wxmacmolplt/tools/chocolateyinstall.ps1
@@ -1,16 +1,21 @@
 ﻿$packageName = 'wxMacMolPlt'
 $version = '7.7'
 
-$InstallArgs = @{
-  packageName = $packageName
-  installerType = 'exe'
-  url = ""
-  url64bit = ''
-  checkSum = ''
-  checkSum64 = ''
-  checkSumType = 'sha256'
-  silentArgs = ''
-  validExitCodes = @(0)
-}
+$InstallDir = Split-path (Split-path $MyInvocation.MyCommand.Definition)
 
-Install-ChocolateyPackage @InstallArgs
+$InstallArgs = @{
+   PackageName = $packageName
+   Url = 'https://uofi.box.com/shared/static/r0jifp0tg0gjc5y51rl9i1dumq3q46nm.zip' 
+   Url64 = 'https://uofi.box.com/shared/static/ah3hq3omjz45jm882n5ye8tzg7tq7i6r.zip'
+   UnzipLocation = $InstallDir
+   checkSum = '391E412CB5AF06E02EF63BD960632D93DDDD5526B68C8A7C0694544FD1DE9757'
+   checkSum64 = '2396D0075A229422D9565F1093BFB6B6B84D4098CD83E544E262E4386F21F5B2'
+   checkSumType = 'sha256'
+}
+Install-ChocolateyZipPackage @InstallArgs
+
+
+$target   = (gci $InstallDir 'wxMacMolPlt.exe' -Recurse).FullName
+$shortcut = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\programs\$packageName.lnk"
+
+Install-ChocolateyShortcut -ShortcutFilePath $shortcut -TargetPath $target
