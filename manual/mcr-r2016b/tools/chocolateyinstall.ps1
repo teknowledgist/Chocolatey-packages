757d3079ae1801bcb1dbcb6f9a29a75c0ee136eb
teknowledgist <teknowledgist@users.noreply.github.com>
2017-03-16 08:13:42 -0400

    establish matlab runtime and update geoda

---
+++ manual/mcr-r2016b/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'mcr-r2016b'
+$Version     = '9.1'
+$url64       = 'https://www.mathworks.com/supportfiles/downloads/R2016b/deployment_files/R2016b/installers/win64/MCR_R2016b_win64_installer.exe'
+$checkSum64  = 'A290F18CD3A21EB09181ED3B0D6A6C9857893FB2321D4F808EEF1F5659ABE20D'
+
+$WorkSpace = Join-Path $env:TEMP "$packageName.$Version"
+
+$WebFileArgs = @{
+   packageName  = $packageName
+   FileFullPath = Join-Path $WorkSpace 'installer.exe'
+   Url64bit     = $url64
+   Checksum64   = $checkSum64
+   ChecksumType = 'sha256'
+   GetOriginalFileName = $true
+}
+
+$msgtext = 'MATLAB Runtime is large, so it may take awhile to download and install.  Please be patient.'
+Write-Host $msgtext -ForegroundColor Cyan
+
+$PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
+
+$UnzipArgs = @{
+   PackageName = $packageName
+   FileFullPath = $PackedInstaller
+   Destination  = $WorkSpace
+}
+
+Get-ChocolateyUnzip @UnzipArgs
+
+$Installer = (Get-ChildItem $WorkSpace -Include 'setup.exe' -Recurse).fullname
+
+$InstallArgs = @{
+   PackageName = $packageName
+   File = $Installer
+   SilentArgs = '-mode silent -agreeToLicense yes'
+   UseOnlyPackageSilentArguments = $true
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
+
---
+++ manual/mcr-r2016b/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'mcr-r2016b'
+$Version = '9.1'
+$softwareName = 'MATLAB Runtime ' + $Version
+
+$RegistryLocation = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+$unexe = Get-ItemProperty "$RegistryLocation\*" | 
+                     Where-Object { $_.displayname -match $softwareName} |
+                     Select-Object -ExpandProperty UninstallString
+
+$InstalledPath = $unexe -replace "^(.*v$($version.replace('.','')))\\.*",'$1'
+$unexe = $unexe.remove(($unexe.LastIndexOf($InstalledPath) - 1))
+
+$UninstallArgs = @{
+   packageName = $packageName
+   fileType = 'exe'
+   file = $unexe
+   silentArgs = '"' + $InstalledPath + '" -mode silent'
+   validExitCodes = @(0)
+}
+
+Uninstall-ChocolateyPackage @UninstallArgs
+
+if (Test-Path $InstalledPath) {
+   Remove-Item $InstalledPath -Recurse
+}
