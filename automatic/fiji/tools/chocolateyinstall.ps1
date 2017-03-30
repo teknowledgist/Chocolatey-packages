a01b15179eb42b3a9284ca05f40f1e40d4395582
teknowledgist <teknowledgist@users.noreply.github.com>
2017-03-30 16:41:30 -0400

    runassystem and fiji work

--- automatic/fiji/tools/chocolateyinstall.ps1
+++ automatic/fiji/tools/chocolateyinstall.ps1
@@ -1,138 +1,105 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿$packageName = 'Fiji'
+$version     = '2.0.0.5900'
+$url         = 'http://downloads.imagej.net/fiji/latest/fiji-nojre.zip'
+$checkSum    = 'A64D32E78A15B514B49D5E82651352BF65345A36B46F289952F4AAF0141E29C1'
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
+$InstallDir = Join-Path $env:ProgramData $packageName
 
-$packageName= 'fiji' # arbitrary name for the package, used in messages
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '' # download url, HTTPS preferred
-$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
+$InstallArgs = @{
+   PackageName   = $packageName
+   Url           = $url
+   UnzipLocation = $InstallDir
+   checkSum      = $checkSum
+   checkSumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @InstallArgs
 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
-  url           = $url
-  url64bit      = $url64
-  #file         = $fileLocation
+$BitLevel = Get-ProcessorBits
+$IJLauncher = Join-Path $InstallDir "Fiji.app\ImageJ-win$BitLevel.exe"
 
-  softwareName  = 'fiji*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+# javaw -Dpatch.ij1=false -Dpython.cachedir.skip=true -Dplugins.dir=C:\\PROGRA~3\\Fiji.app -Xmx3072m -Xincgc -XX:PermSize=128m -Djava.class.path=C:\\PROGRA~3\\Fiji.app/jars/imagej-launcher-4.0.5.jar -Dimagej.dir=C:\\PROGRA~3\\Fiji.app -Dij.dir=C:\\PROGRA~3\\Fiji.app -Dfiji.dir=C:\\PROGRA~3\\Fiji.app -Dfiji.defaultLibPath=bin/server/jvm.dll -Dfiji.executable=C:\\ProgramData\\Fiji.app\\ImageJ-win64.exe -Dij.executable=C:\\ProgramData\\Fiji.app\\ImageJ-win64.exe -Djava.library.path=C:\\PROGRA~3\\Fiji.app/lib/win64;C:\\PROGRA~3\\Fiji.app/mm/win64 -Dscijava.context.strict=false -Dscijava.log.level=info net.imagej.launcher.ClassLauncher -classpath . -ijjarpath jars -ijjarpath plugins net.imagej.updater.CommandLine update
 
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided. 
-  # You can also use checksum.exe (choco install checksum) and use it 
-  # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
 
-  #MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  #OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s -u'        # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
+# ImageJ/Fiji updates frequently and as pieces/plugins can update without the main program
+#   being assigned a new version number (for Chocolatey to recognize). At this point in 
+#   its development, Fiji is probably better off updating itself.  However, with a proper 
+#   machine-level install, unprivileged users will not be able to update Fiji.
+# The following will establish a scheduled task running as SYSTEM to have ImageJ/Fiji 
+#   check for updates on a weekly basis.
+$TaskName = "Update Fiji"
+$TaskService = new-object -com("Schedule.Service")
+$TaskService.connect()
+
+if ((@($TaskService.getfolder("\").gettasks(1)) |select -expandproperty name) -icontains $TaskName) {
+   $TaskService.getfolder("\").DeleteTask($TaskName,0)
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
-#Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
-## If you are making your own internal packages (organizations), you can embed the installer or 
-## put on internal file share and use the following instead (you'll need to add $file to the above)
-#Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
+$task_xml = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.3" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Date>$(get-date -Format yyyy-MM-ddTHH:MM:ss.00000)</Date>
+    <Author>Chocolatey</Author>
+    <Description>Periodically checks for updates to (Chocolatey-installed) Fiji.  If this task is disabled or removed, the instance of Fiji installed by Chocolatey will not automatically update.</Description>
+  </RegistrationInfo>
+  <Triggers>
+    <CalendarTrigger>
+      <StartBoundary>$(get-date -Format yyyy-MM-ddTHH:MM:00)</StartBoundary>
+      <Enabled>true</Enabled>
+      <ScheduleByDay>
+        <DaysInterval>7</DaysInterval>
+      </ScheduleByDay>
+    </CalendarTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <RunLevel>LeastPrivilege</RunLevel>
+      <GroupId>NT AUTHORITY\SYSTEM</GroupId>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>StopExisting</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>true</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <UseUnifiedSchedulingEngine>false</UseUnifiedSchedulingEngine>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>PT1H</ExecutionTimeLimit>
+    <Priority>9</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>cmd.exe</Command>
+      <Arguments>/c "path=%path%;c:\programdata\oracle\java\javapath &#038; c:\programdata\Fiji.app\ImageJ-win64.exe --update update"</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+"@
 
-## Main helper functions - these have error handling tucked into them already
-## see https://chocolatey.org/docs/helpers-reference
+$Task = $TaskService.NewTask($null)
+$task.XmlText = $task_xml
 
-## Install an application, will assert administrative rights
-## - https://chocolatey.org/docs/helpers-install-chocolatey-package
-## - https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-## add additional optional arguments as necessary
-##Install-ChocolateyPackage $packageName $fileType $silentArgs $url [$url64 -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+$TaskService.getfolder("\").RegisterTaskDefinition($TaskName, $Task, 6, $null, $null, 5) > $null
 
-## Download and unpack a zip file - https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
-##Install-ChocolateyZipPackage $packageName $url $toolsDir [$url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+Write-Host 'Making sure installed version if fully patched.  Please be patient' -ForegroundColor Cyan
+# ($TaskService.GetFolder("\").GetTasks(1) | ? {$_.name -eq $TaskName}).Run(0)
+Write-Host 'If Java was just installed, future updates to Fiji may not occur until the computer is rebooted.' -ForegroundColor Cyan
 
-## Install Visual Studio Package - https://chocolatey.org/docs/helpers-install-chocolatey-vsix-package
-#Install-ChocolateyVsixPackage $packageName $url [$vsVersion] [-checksum $checksum -checksumType $checksumType]
-#Install-ChocolateyVsixPackage @packageArgs
 
-## see the full list at https://chocolatey.org/docs/helpers-reference
 
-## downloader that the main helpers use to download items
-## if removing $url64, please remove from here
-## - https://chocolatey.org/docs/helpers-get-chocolatey-web-file
-#Get-ChocolateyWebFile $packageName 'DOWNLOAD_TO_FILE_FULL_PATH' $url $url64
+$FijiLauncher = $IJLauncher -replace 'ImageJ','fiji'
+$DeskShortcut = Join-Path ([System.Environment]::GetFolderPath('Desktop')) 'Fiji.lnk'
+$StartShortcut = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Fiji.lnk'
 
-## Installer, will assert administrative rights - used by Install-ChocolateyPackage
-## use this for embedding installers in the package when not going to community feed or when you have distribution rights
-## - https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-#Install-ChocolateyInstallPackage $packageName $fileType $silentArgs '_FULLFILEPATH_' -validExitCodes $validExitCodes
-
-## Unzips a file to the specified location - auto overwrites existing content
-## - https://chocolatey.org/docs/helpers-get-chocolatey-unzip
-#Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" $toolsDir
-
-## Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-## - https://chocolatey.org/docs/helpers-start-chocolatey-process-as-admin
-#Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
-
-## add specific folders to the path - any executables found in the chocolatey package 
-## folder will already be on the path. This is used in addition to that or for cases 
-## when a native installer doesn't add things to the path.
-## - https://chocolatey.org/docs/helpers-install-chocolatey-path
-#Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
-
-## Add specific files as shortcuts to the desktop
-## - https://chocolatey.org/docs/helpers-install-chocolatey-shortcut
-#$target = Join-Path $toolsDir "$($packageName).exe"
-# Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>" [-workDirectory "C:\" -arguments "C:\test.txt" -iconLocation "C:\test.ico" -description "This is the description"]
-
-## Outputs the bitness of the OS (either "32" or "64")
-## - https://chocolatey.org/docs/helpers-get-o-s-architecture-width
-#$osBitness = Get-ProcessorBits
-
-## Set persistent Environment variables
-## - https://chocolatey.org/docs/helpers-install-chocolatey-environment-variable
-#Install-ChocolateyEnvironmentVariable -variableName "SOMEVAR" -variableValue "value" [-variableType = 'Machine' #Defaults to 'User']
-
-## Set up a file association
-## - https://chocolatey.org/docs/helpers-install-chocolatey-file-association
-#Install-ChocolateyFileAssociation 
-
-## Adding a shim when not automatically found - Cocolatey automatically shims exe files found in package directory.
-## - https://chocolatey.org/docs/helpers-install-bin-file
-## - https://chocolatey.org/docs/create-packages#how-do-i-exclude-executables-from-getting-shims
-#Install-BinFile
-
-##PORTABLE EXAMPLE
-#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
-#Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
-## END PORTABLE EXAMPLE
-
-## [DEPRECATING] PORTABLE EXAMPLE
-#$binRoot = Get-BinRoot
-#$installDir = Join-Path $binRoot "$packageName"
-#Write-Host "Adding `'$installDir`' to the path and the current shell path"
-#Install-ChocolateyPath "$installDir"
-#$env:Path = "$($env:Path);$installDir"
-
-# if removing $url64, please remove from here
-# despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
-#Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
-## END PORTABLE EXAMPLE
+Install-ChocolateyShortcut -ShortcutFilePath $DeskShortcut -TargetPath $FijiLauncher
+Install-ChocolateyShortcut -ShortcutFilePath $StartShortcut -TargetPath $FijiLauncher
---
+++ manual/runassystem/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+   packageName   = 'runassystem'
+   FileFullPath  = Join-Path (Split-Path (Split-Path -parent $MyInvocation.MyCommand.Definition)) 'runassystem.exe'
+   Url           = 'https://www.apreltech.com/Downloads/runassystem.exe'
+   CheckSum      = '085AD59BB8D32981EA590A7884DA55D4B0A3F5E89A9632530C0C8EF2F379E471'
+   CheckSumType  = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+$ShortcutArgs = @{
+   ShortcutFilePath = Join-Path ([System.Environment]::GetFolderPath('Desktop')) 'RunAsSystem.lnk'
+   TargetPath       = $packageArgs.FileFullPath
+   IconLocation     = Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) 'runassystem.ico'
+}
+
+Install-ChocolateyShortcut @ShortcutArgs
+
