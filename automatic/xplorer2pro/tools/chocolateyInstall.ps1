2089db4c1be15878bdc5607059833eb60fcf3f61
teknowledgist <teknowledgist@gmail.com>
2017-06-23 22:52:31 -0400

    path fix

---
+++ automatic/xplorer2pro/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿
+$packageName = 'xplorer2pro'
+$installerType = 'exe'
+$url = 'http://zabkat.com/xplorer2_setup.exe'
+$url64 = 'http://zabkat.com/xplorer2_setup64.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
