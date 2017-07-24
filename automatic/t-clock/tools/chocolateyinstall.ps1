b87884dda97e0df04817f285e9c1daced7136d72
teknowledgist <teknowledgist@users.noreply.github.com>
2017-07-24 12:08:59 -0400

    create t-clock

---
+++ automatic/t-clock/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$Version = '2.4.3.471'
+
+$packageArgs = @{
+  url           = 'https://github.com/White-Tiger/T-Clock/releases/download/v2.4.3%23471-beta/T-Clock.7z'
+  checksum      = '16054119b2a21afdb2fe028ce776c3cd02b2d31a4cde6d9f0574e31a405d8d15'
+  checksumType  = 'sha256'
+  unzipLocation = Join-Path (Split-Path (Split-Path $MyInvocation.MyCommand.Definition)) "v$Version"
+  packageName   = $env:ChocolateyPackageName
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$exes = Get-ChildItem $packageArgs.UnzipLocation -filter *.exe -Recurse |select -ExpandProperty fullname
+foreach ($exe in $exes) {
+   if ($exe -notmatch 'clock(64)?.exe$') {
+      New-Item "$exe.ignore" -Type file -Force | Out-Null
+   }
+}
