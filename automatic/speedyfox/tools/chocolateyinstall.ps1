f643811c403308fd4921ec69e9ffc600d2c6fd7c
teknowledgist <teknowledgist@users.noreply.github.com>
2017-09-01 17:06:46 -0400

    added speedyfox

---
+++ automatic/speedyfox/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+   packageName   = $env:ChocolateyPackageName
+   unzipLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
+   url           = 'https://www.crystalidea.com/downloads/speedyfox.zip'
+   checksum      = 'd3f416a68d5df07d0b26b58974431f7039d5537d2af620745211195c6bf35b32'
+   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/speedyfox/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+$Release = 'https://www.crystalidea.com/speedyfox/release-notes'
+
+function global:au_GetLatest {
+   $download_page = Invoke-WebRequest -Uri $Release
+
+   $VerstionText = $download_page.AllElements | ? {$_.tagname -eq 'h2'} |select -First 1 -ExpandProperty innertext
+   $Version = $VerstionText.trim().split()[-1]
+
+   $url = 'https://www.crystalidea.com/downloads/speedyfox.zip'
+
+   return @{ Version = $Version; URL = $url }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\chocolateyInstall.ps1" = @{
+         "(^   Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      }
+   }
+}
+
+Update-Package -ChecksumFor 32
