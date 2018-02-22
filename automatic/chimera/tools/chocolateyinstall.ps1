bd4c2d069ad3c983f6a1a6951e0c8206ff39fdcf
teknowledgist <teknowledgist@users.noreply.github.com>
2018-02-22 13:23:08 -0500

    added chimera

---
+++ automatic/chimera/tools/chocolateyinstall.ps1
@@ -0,0 +1,49 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$BaseURL = 'https://www.cgl.ucsf.edu'
+
+# Discovering download link
+# Process found here:  https://stackoverflow.com/questions/34422255/
+# Unsure if all settings are needed.
+$url = "$BaseURL/chimera/cgi-bin/secure/chimera-get.py"
+$postData = "choice=Accept&file=win64/chimera-$env:ChocolateyPackageVersion-win64.exe"
+$buffer = [text.encoding]::ascii.getbytes($postData)
+[net.httpWebRequest] $req = [net.webRequest]::create($url)
+$req.method = "POST"
+$req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+$req.Headers.Add("Accept-Language: en-US")
+$req.Headers.Add("Accept-Encoding: gzip,deflate")
+$req.Headers.Add("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7")
+$req.AllowAutoRedirect = $false
+$req.ContentType = "application/x-www-form-urlencoded"
+$req.ContentLength = $buffer.length
+$req.TimeOut = 50000
+$req.KeepAlive = $true
+$req.Headers.Add("Keep-Alive: 300");
+$reqst = $req.getRequestStream()
+$reqst.write($buffer, 0, $buffer.length)
+$reqst.flush()
+$reqst.close()
+[net.httpWebResponse] $res = $req.getResponse()
+$resst = $res.getResponseStream()
+$sr = new-object IO.StreamReader($resst)
+$result = $sr.ReadToEnd()
+$res.close()
+
+$URLstub = ($result.split() |? {$_ -match 'href='}) -replace '.*href="(.*)".*','$1'
+
+Write-Host 'You are establishing a license agreement as defined here:' -ForegroundColor Cyan
+Write-Host 'http://www.cgl.ucsf.edu/chimera/license.html' -ForegroundColor Cyan
+
+$packageArgs = @{
+   packageName   = $env:ChocolateyPackageName
+   fileType      = 'EXE'
+   url64bit      = $BaseURL + $URLstub
+   softwareName  = 'UCSF Chimera*' 
+   checksum64    = 'e0d294db3dd9c541b70ad7d1483bc324150af7e2dc04b9ad94e4e04c6e8847ab'
+   checksumType64= 'sha256'
+   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+   validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/chimera/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+function global:au_GetLatest {
+   $BaseURL = 'https://www.cgl.ucsf.edu'
+
+   $download_page = Invoke-WebRequest -Uri "$BaseURL/chimera/download.html"
+
+   $HREF = $download_page.links | 
+                  Where-Object {$_.href -match '([0-9.]+)-win64.exe'} | 
+                  Select-Object -ExpandProperty href
+
+   $version = $Matches[1]
+
+   $body = 'choice=Accept&' + $HREF.split('?')[-1]
+   $URI = $BaseURL + ($HREF.split('?')[0])
+   $result = Invoke-WebRequest $URI -body $body -Method Post
+
+   $url64 = $BaseURL + ($result.links |Select-Object -ExpandProperty href)
+
+   return @{ 
+      Version = $version
+      URL64 = $url64 
+   }
+}
+
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update -ChecksumFor 64
