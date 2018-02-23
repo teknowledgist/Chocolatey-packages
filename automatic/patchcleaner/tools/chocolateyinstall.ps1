0edaf9d3be43630446cb416a60642ebacbc87171
teknowledgist <teknowledgist@users.noreply.github.com>
2018-02-23 16:28:16 -0500

    submit patchcleaner

---
+++ automatic/patchcleaner/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$fileLocation = (Get-ChildItem (Split-Path $MyInvocation.MyCommand.Definition) -Filter '*.exe').FullName
+
+$UnzipDir = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+# Extract zip
+Get-ChocolateyUnzip -FileFullPath $fileLocation -Destination $UnzipDir
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   softwareName   = "$env:ChocolateyPackageName*"
+   fileType       = 'msi'
+   File           = (Get-ChildItem $UnzipDir -filter "*.msi" -Recurse).FullName
+   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
+
+New-Item "$fileLocation.ignore" -Type file -Force | Out-Null
+
---
+++ automatic/patchcleaner/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+function global:au_GetLatest {
+   $Release = 'http://sourceforge.net/projects/patchcleaner/files'
+
+   $download_page = Invoke-WebRequest -Uri $Release
+
+   $URLstub = ($download_page.links |
+                 Where-Object {$_.href -match 'latest'} | 
+                 Select-Object -ExpandProperty title).split(':')[0]
+
+   $version = $URLstub -replace '.*_([0-9.]+)\.[^0-9].*','$1'
+
+   $url = $Release.replace('http:','https:') + $URLstub
+
+   return @{ Version = $version; URL32 = $url }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      'tools\VERIFICATION.txt' = @{
+         '(^Version\s+:).*'      = "`${1} $($Latest.Version)"
+         '(^URL\s+:).*'          = "`${1} $($Latest.URL32)"
+         '(^Checksum\s+:).*'     = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading PatchCleaner $($Latest.Version) installer."
+   Get-RemoteFiles -Purge -NoSuffix
+}
+
+update -ChecksumFor none
