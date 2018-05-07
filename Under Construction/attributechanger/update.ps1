ea4504cb496b97a25e50054f2226890323dbb9e0
teknowledgist <teknowledgist@users.noreply.github.com>
2018-05-06 22:35:08 -0400

    ac work

---
+++ Under Construction/attributechanger/update.ps1
@@ -0,0 +1,43 @@
+import-module au
+
+
+function global:au_GetLatest {
+   $DownloadURI = 'https://www.petges.lu/download/'
+   $download_page = Invoke-WebRequest -Uri $DownloadURI
+
+   $Link = $download_page.links |? {$_.href -match '\.exe'} | select -First 1
+   
+   $version = $Link.innerText.split()[-1]
+   
+   if ($version -match '[a-z]') {
+      $NumEquiv = ([byte][char]$matches[0])-96
+      $version = $version -replace '[a-z]',".$NumEquiv"
+   }
+
+   $url32 = $Link.href
+
+   return @{ 
+            Version = $version
+            URL32   = $url32
+           }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\VERIFICATION.txt" = @{
+         "(^Version\s+:).*"      = "`${1} $($Latest.Version)"
+         "(^URL\s+:).*"          = "`${1} $($Latest.URL32)"
+         "(^Checksum\s+:).*"     = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading Attribute Changer $($Latest.Version)"
+   Get-RemoteFiles -Purge -NoSuffix
+}
+
+
+update -ChecksumFor none
+if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
