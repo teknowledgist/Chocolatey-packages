d1802fe7c24321f77d4b48c0e75de608ba06584c
teknowledgist <teknowledgist@users.noreply.github.com>
2018-08-15 09:30:42 -0400

    MATLAB r2015a

---
+++ manual/mcr-r2015a/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$WorkSpace = Join-Path $env:TEMP "$env:ChocolateyPackageName.$env:ChocolateyPackageVersion"
+
+$WebFileArgs = @{
+   packageName  = $env:ChocolateyPackageName
+   FileFullPath = Join-Path $WorkSpace 'installer.exe'
+   Url          = 'https://ssd.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/win32/MCR_R2015a_win32_installer.exe'
+   Url64bit     = 'https://ssd.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/win64/MCR_R2015a_win64_installer.exe'
+   Checksum     = '109f90312e467a101d0dd9787a1fbcb3ea38a07f32a7b960dfd4cfa1b7ad00a6'
+   Checksum64   = '6e265e0619851a86baa5f5edcae836cc68046977ecb00eeb6085e5a4f98ff812'
+   ChecksumType = 'sha256'
+   GetOriginalFileName = $true
+}
+
+$msgtext = 'MATLAB Runtime is large, so it may take awhile to download and install.  Please be patient.'
+Write-Host $msgtext -ForegroundColor Cyan
+
+$PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
+
+$UnzipArgs = @{
+   PackageName = $env:ChocolateyPackageName
+   FileFullPath = $PackedInstaller
+   Destination  = $WorkSpace
+}
+
+Get-ChocolateyUnzip @UnzipArgs
+
+$Installer = (Get-ChildItem $WorkSpace -Include 'setup.exe' -Recurse).fullname
+
+$InstallArgs = @{
+   PackageName = $env:ChocolateyPackageName
+   File = $Installer
+   SilentArgs = '-mode silent -agreeToLicense yes'
+   UseOnlyPackageSilentArguments = $true
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
+
---
+++ manual/mcr-r2015a/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$SoftwareName = "MATLAB Runtime $env:ChocolateyPackageVersion"
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $SoftwareName
+
+if ($key.Count -eq 1) {
+   $UninstallArgs = @{
+      packageName    = $env:ChocolateyPackageName
+      softwareName   = $SoftwareName
+      fileType       = 'exe'
+      file           = $key[0].UninstallString -replace '(.*\.exe).*','$1'
+      silentArgs     = '"' + $key[0].InstallLocation + '" -mode silent'
+      validExitCodes = @(0)
+   }
+   Uninstall-ChocolateyPackage @UninstallArgs
+   if (Test-Path $key[0].InstallLocation) {
+      Remove-Item $key[0].InstallLocation -Recurse
+   }
+} elseif ($key.Count -eq 0) {
+   Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+   Write-Warning "$($key.Count) matches found!"
+   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+   Write-Warning "Please alert package maintainer the following keys were matched:"
+   $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
