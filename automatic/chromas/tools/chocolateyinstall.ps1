dbcca1de66271ed0fa01a07f297bb2eb04a0c9d2
teknowledgist <teknowledgist@users.noreply.github.com>
2018-10-10 17:12:46 -0400

    moving to jsdeliver for icons

---
+++ automatic/chromas/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   installerType  = 'exe'
+   url            = 'http://www.technelysium.com.au/Chromas265Setup.exe'
+   Checksum       = '3d8cc4dccce577a6ba147d552d942a840fa83db48590d0922f45ab0bda7871c3'
+   ChecksumType   = 'sha256'
+   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
+
