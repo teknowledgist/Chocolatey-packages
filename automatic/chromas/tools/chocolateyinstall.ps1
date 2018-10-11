7b6338e0b715a20b7c1779bba48089478a4474be
teknowledgist <teknowledgist@users.noreply.github.com>
2018-10-11 09:28:02 -0400

    chromas and jsdelivr moves

--- automatic/chromas/tools/chocolateyinstall.ps1
+++ automatic/chromas/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.exe').FullName
+
 $InstallArgs = @{
    packageName    = $env:ChocolateyPackageName
-   installerType  = 'exe'
-   url            = 'http://www.technelysium.com.au/Chromas265Setup.exe'
-   Checksum       = '3d8cc4dccce577a6ba147d552d942a840fa83db48590d0922f45ab0bda7871c3'
-   ChecksumType   = 'sha256'
-   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+   FileType       = 'exe'
+   File           = $fileLocation
+   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
    validExitCodes = @(0)
 }
 
-Install-ChocolateyPackage @InstallArgs
-
+Install-ChocolateyInstallPackage @InstallArgs
+New-Item "$fileLocation.ignore" -Type file -Force | Out-Null
---
+++ automatic/chromas/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+function global:au_GetLatest {
+   $DownloadURI = 'https://technelysium.com.au/wp/chromas/'
+   $download_page = Invoke-WebRequest -Uri $DownloadURI
+
+   $Link = $download_page.links |? {($_.href -match '\.exe') -and ($_.outertext -match 'free')}
+   
+   $version = $Link.innerText.split() | ? {$_ -match '^[0-9.]+$'}
+
+   $url32 = $Link.href
+
+   return @{ 
+      Version  = $version
+      URL32    = $url32
+   }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\VERIFICATION.txt" = @{
+         "(^Version\s+:).*"      = "`${1} $($Latest.Version)"
+         "(^URL\s+:).*"          = "`${1} $($Latest.URL32)"
+         "(^Checksum\s+:).*"     = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading Chromas $($Latest.Version)"
+   Get-RemoteFiles -Purge -NoSuffix
+}
+
+
+update -ChecksumFor none
+if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
