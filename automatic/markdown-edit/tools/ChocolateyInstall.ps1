25d44af5d007013432773a5299b4664c978a0523
teknowledgist <teknowledgist@users.noreply.github.com>
2018-10-15 12:52:21 -0400

    Icon changes

--- automatic/markdown-edit/tools/ChocolateyInstall.ps1
+++ automatic/markdown-edit/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$PackageName = 'Markdown Edit'
 # Lines to be updated by automatic updater
-$Url = 'https://github.com/mike-ward/Markdown-Edit/releases/download/v1.34/MarkdownEditSetup.msi'
-$Checksum = 'd237e53f1ededce4851409bae79a8f610cc9e03cd91ca96bd5280536d95602a7'
+$Url = 'https://github.com/mike-ward/Markdown-Edit/releases/download/v1.35/MarkdownEditSetup.msi'
+$Checksum = '234656b63192e7c5042714c557d1df9b208d0c28a2c9a8cee4a0739e144068e1'
 
 $OSversion = [version](Get-WmiObject -Class Win32_OperatingSystem).version 
 if ($OSversion -eq $null -or $OSversion -lt [Version]'6.2') {
--- automatic/speedyfox/tools/chocolateyinstall.ps1
+++ automatic/speedyfox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    unzipLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
    url           = 'https://www.crystalidea.com/downloads/speedyfox.zip'
-   checksum      = '4cb018484f31c4537be81e10426a438eff3b2d60d7ee099c9a54e0c2632494b2'
+   checksum      = 'b9bf840d66be1e481bef922b51d0c308f174216e361b9f8144cc584e7e40f224'
    checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
