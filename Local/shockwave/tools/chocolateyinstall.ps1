4de874c4b687442e032a16184ea4483643420078
teknowledgist <teknowledgist@users.noreply.github.com>
2018-11-21 10:14:57 -0500

    Shockwave

--- Local/shockwave/tools/chocolateyinstall.ps1
+++ Local/shockwave/tools/chocolateyinstall.ps1
@@ -1,15 +1,24 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿# First, uninstall all EXE installed versions
+[array]$keys = Get-UninstallRegistryKey -SoftwareName 'Adobe Shockwave Player*'
 
-$packageName= $env:ChocolateyPackageName
+Foreach ($Key in $Keys) {
+   if ($Key.UninstallString -notmatch 'msiexec') {
+      $Uninstall = $Key.UninstallString
+      Start-Process $Uninstall -wait -ArgumentList '/S'
+   }
+}
+
+$ErrorActionPreference = 'Stop'
+
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
-$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.exe').FullName
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.msi').FullName
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE' 
+   packageName   = $env:ChocolateyPackageName
+   fileType      = 'msi' 
   file          = $fileLocation
-  softwareName  = "$env:ChocolateyPackageName*"
-  silentArgs   = '/S'
+   softwareName  = "Adobe Shockwave Player*"
+   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
 Install-ChocolateyInstallPackage @packageArgs
--- Local/shockwave/update.ps1
+++ Local/shockwave/update.ps1
@@ -4,17 +4,24 @@ function global:au_GetLatest {
    $Release = 'https://helpx.adobe.com/shockwave/release-note/release-notes-shockwave-12.html'
 
    $Page = Invoke-WebRequest -Uri $Release
-   $Version = $Page.AllElements | 
-                  Where-Object {$_.tagname -eq 'li' -and $_.innertext -match '^[\d\.]+$'} |
-                  Select-Object -ExpandProperty innertext -First 1
+   $Version = $page.RawContent -split '</?li>' | 
+                  Where-Object {$_ -match '^(\d|\.)+$'} | 
+                  Select-Object -First 1
 
    # You must go to http://www.adobe.com/products/players/fpsh_distribution1.html to request distribution rights.
    # The actual download page for Shockwave cannot be shared under the agreement
+   $Downloads = '<redacted>'
+
+   $DownloadPage = Invoke-WebRequest -Uri $Downloads
+   $DownloadStub = $DownloadPage.Links |
+                     Where-Object {$_.innertext -match 'MSI Installer'} |
+                     Select-Object -ExpandProperty href -First 1
+   $DownloadURL = "https://www.adobe.com$DownloadStub"
 
    return @{ 
             Version  = $Version
-            URL32    = ''  # From the Software Distribution page
-            FileType = 'exe'
+            URL32    = $DownloadURL
+            FileType = 'msi'
            }
 }
 
@@ -25,7 +32,7 @@ function global:au_SearchReplace {
 
 function global:au_BeforeUpdate() { 
    Write-host "Downloading Shockwave v$($Latest.Version)"
-   Get-RemoteFiles -Purge -NoSuffix
+   Get-RemoteFiles -Purge -NoSuffix -FileNameBase "sw_lic_full_installer_v$($Latest.Version)"
 }
 
 update -ChecksumFor none
