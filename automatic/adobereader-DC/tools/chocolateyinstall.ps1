db24b53be16f2394ad45e085ee0010d5976c6082
teknowledgist <teknowledgist@users.noreply.github.com>
2018-12-03 17:19:35 -0500

    Playing with adobereader

--- automatic/adobereader-DC/tools/chocolateyinstall.ps1
+++ automatic/adobereader-DC/tools/chocolateyinstall.ps1
@@ -1,38 +1,151 @@
-﻿# adobe-acrobat-reader-dc install
+﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$DisplayName = 'Adobe Acrobat Reader DC MUI'
+
+$MUIurl            = ''
+$MUIchecksum       = ''
+$MUImspURL         = ''
+$MUImspChecksum    = ''
+
+$MUIinstalled = $false
+$UpdateOnly = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $DisplayName.replace(' MUI','*')
+
+if ($key.Count -eq 1) {
+   $InstalledVersion = $key[0].DisplayVersion.replace('.','')
+   $InstallerVersion = (Split-Path $MUIurl -Leaf) -replace '[^0-9]',''
+   if ($key[0].DisplayName -notmatch 'MUI') {
+      if ($InstalledVersion -ge $InstallerVersion) {
+         Write-Warning "The currently installed $($key[0].DisplayName) is a single-language install."
+         Write-Warning "This multi-language (MUI) package cannot overwrite it at this time."
+         Write-Warning "You will need to uninstall $($key[0].DisplayName) first."
+         Throw "Installation halted."
+      } else {
+         Write-Warning "The currently installed $($key[0].DisplayName) is a single-language install."
+         Write-Warning  "This package will replace it with the multi-language (MUI) release."
+      }
+   } else {
+      $MUIinstalled = $true
+      $UpdaterVersion = (Split-Path $MUImspURL -Leaf) -replace '[^0-9]',''
+      if ($InstalledVersion -eq $UpdaterVersion) {
+         Write-Verbose "Currently installed version is the same as this package.  Nothing further to do."
+         Return
+      } elseif ($InstalledVersion -gt $UpdaterVersion) {
+         Write-Warning "$($key[0].DisplayName) v20$($key[0].DisplayVersion) installed."
+         Write-Warning "This package installs v$env:ChocolateyPackageVersion and cannot replace a newer version."
+         Throw "Installation halted."
+      } elseif (($InstalledVersion -ge $InstallerVersion) -and ($InstalledVersion -lt $UpdaterVersion)) {
+         $UpdateOnly = $true
+      }
+   }
+} elseif ($key.count -gt 1) {
+   Write-Warning "$($key.Count) matching installs of Adobe Acrobat Reader DC found!"
+   Write-Warning "To prevent accidental data loss, this install will be aborted."
+   Write-Warning "The following installs were found:"
+   $key | % {Write-Warning "- $($_.DisplayName)`t$($_.DisplayVersion)"}
+   Throw "Installation halted."
+}
+
 $PackageParameters   = Get-PackageParameters
-$urlPackageEXE       = 'http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1900820071/AcroRdrDC1900820071_MUI.exe'
-$urlPackageMSP       = 'http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1900820081/AcroRdrDCUpd1900820081_MUI.msp'
-$checksumPackageEXE  = '0fea79f1fe7fa407846e6253c10f8739cbb6532b7181a7f8d3c2538ead8d6fbb97faa72334df7d2af9e058d1b15c7ced5e57a36733cf8678de959081cbbd0727'
-$checksumPackageMSP  = '90D5B0101F17918B2EC36ABB40ED1F04703E46101513EFDDB51D5E3CB73519950434440149700CCD737DCB4B1FAEFEFE4D4707C46BC9C96FE5BA7A6C4365B8D3'
-$checksumTypePackage = 'SHA512'
-$binDir              = "$($toolsDir)\bin"
-$mspPath             = Join-Path $binDir "AcroRdrDCUpd$($env:ChocolateyPackageVersion).msp"
-
-Import-Module -Name "$($toolsDir)\helpers.ps1"
-
-$packageArgsEXE = @{
-    packageName    = $env:ChocolateyPackageName
-    fileType       = 'EXE'
-    url            = $urlPackageEXE
-    checksum       = $checksumPackageEXE
-    checksumType   = $checksumTypePackage
-	silentArgs     = '/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES'
-    validExitCodes = @(0, 1000, 1101)
+# Reference: https://www.adobe.com/devnet-docs/acrobatetk/tools/AdminGuide/properties.html#command-line-example
+$options = ' DISABLEDESKTOPSHORTCUT=1'
+if ($PackageParameters.DesktopIcon) {
+   $options += ''
+   Write-Host 'You requested a desktop icon.' -ForegroundColor Cyan
 }
 
-#if (-Not (Get-ProductName("Adobe Acrobat Reader DC MUI"))) {
-    Install-ChocolateyPackage @packageArgsEXE
-#}
-
-$args = "/p $mspPath /quiet"
-Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath $mspPath -Url $urlPackageMSP -Checksum $checksumPackageMSP -ChecksumType $checksumTypePackage
-Start-ChocolateyProcessAsAdmin $args 'msiexec'
-
-if ($PackageParameters.RemoveDesktopIcons) {
-    Remove-DesktopIcons -Name "Acrobat Reader DC" -Desktop "Public"
+if ($PackageParameters.NoUpdates) {
+   $RegRoot = 'HKLM:\SOFTWARE\Policies'
+   $RegSubFolders = ('Adobe\Acrobat Reader\DC\FeatureLockDown').split('\')
+   for ($i=0; $i -lt $RegSubFolders.count; $i++) {
+      $RegPath = "$RegRoot\$($RegSubFolders[0..$i] -join '\')"
+      if (-not (Test-Path $RegPath)) {
+         $null = New-Item -Path $RegPath.TrimEnd($RegSubFolders[$i]) -Name $RegSubFolders[$i]
+      }
+   }
+   $RegPath = "$RegRoot\$($RegSubFolders -join '\')"
+   if (Test-Path $RegPath) {
+      $null = New-ItemProperty -Path $RegPath -Name 'bUpdater' -PropertyType DWORD -Value 0 -Force
+   }
+   Write-Host 'You requested no Adobe updates.' -ForegroundColor Cyan
 }
+
+if ($PackageParameters.EnableUpdateService) {
+   Write-Host 'You requested to enable the auto-update service.' -ForegroundColor Cyan
+} else {
+   $options += ' DISABLE_ARM_SERVICE_INSTALL=1'
+   $null = Stop-Service "AdobeARMservice" -force
+   $null = Set-Service "AdobeARMservice" -StartupType Disabled
+}
+
+if (-not $PackageParameters.UpdateMode) {
+   $UpdateMode = 0
+} else {$UpdateMode = $PackageParameters.UpdateMode}
+
+if ((0..4) -contains $UpdateMode) {
+   if ($MUIinstalled) {
+      $RegPath1 = 'HKLM:\SOFTWARE\Adobe\Adobe ARM\1.0\ARM\'
+      if (Test-Path $RegPath1) {
+         $null = New-ItemProperty -Path $RegPath1 -Name ' iCheckReader' -Value $UpdateMode -force
+      }
+      $RegPath2 = 'HKLM:\SOFTWARE\Wow6432Node\Adobe\Adobe ARM\Legacy\Reader\{AC76BA86-7AD7-FFFF-7B44-AC0F074E4100}'
+      if (Test-Path $RegPath2) {
+         $null = New-ItemProperty -Path $RegPath2 -Name ' iCheckReader' -Value $UpdateMode -force
+      }
+   } else {
+      $options += " UPDATE_MODE=$UpdateMode"
+      Switch ($UpdateMode) {
+         0 { Write-Host 'You requested manual update checks and installs.' -ForegroundColor Cyan }
+         1 { Write-Host 'You requested manual update checks and installs.' -ForegroundColor Cyan }
+         2 { Write-Host 'You requested automatic update downloads and manual installs.' -ForegroundColor Cyan }
+         3 { Write-Host 'You requested scheduled, automatic updates.' -ForegroundColor Cyan }
+         4 { Write-Host 'You requested notifications but manual updates.' -ForegroundColor Cyan }
+      }
+   }
+}
+
+if (-not $UpdateOnly) {
+   $packageArgsEXE = @{
+      packageName    = $env:ChocolateyPackageName
+      fileType       = 'EXE'
+      url            = $MUIurl
+      checksum       = $MUIchecksum
+      checksumType   = 'SHA256'
+      silentArgs     = "/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES $options" +
+                        " /L*v `"$env:TEMP\$env:chocolateyPackageName.$env:chocolateyPackageVersion.Install.log`""
+      validExitCodes = @(0, 1000, 1101, 1603)
+   }
+   $exitCode = Install-ChocolateyPackage @packageArgsEXE
+   
+   if ($exitCode -eq 1603) {
+      Write-Warning "For code 1603, Adobe recommends to 'shut down Microsoft Office and all web browsers' and try again."
+      Throw "Installation of $env:ChocolateyPackageName was unsuccessful."
+   }
+}
+
+$DownloadArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = Join-Path $env:TEMP "$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.msp"
+   url            = $MUImspURL
+   checksum       = $MUImspChecksum
+   checksumType   = 'SHA256'
+   GetOriginalFileName = $true
+}
+$mspPath = Get-ChocolateyWebFile @DownloadArgs
+
+$UpdateArgs = @{
+   Statements     = "/p $mspPath /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES $options" +
+                        " /L*v `"$env:TEMP\$env:chocolateyPackageName.$env:chocolateyPackageVersion.Update.log`""
+   ExetoRun       = 'msiexec.exe'
+   validExitCodes = @(0, 1603)
+}
+$exitCode = Start-ChocolateyProcessAsAdmin @UpdateArgs
+if ($exitCode -eq 1603) {
+   Write-Warning "For code 1603, Adobe recommends to 'shut down Microsoft Office and all web browsers' and try again."
+   Throw "Installation of $env:ChocolateyPackageName was unsuccessful."
+}
+
+
 
 if ($PackageParameters.EnableAutoUpdate) {
     SchTasks /Change /Enable /TN "Adobe Acrobat Update Task" | Out-Null
--- automatic/adobereader-DC/update.ps1
+++ automatic/adobereader-DC/update.ps1
@@ -1,7 +1,49 @@
 import-module au
 
-$url                    = 'https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html'
-$checksumTypePackageMSP = "SHA512"
+function global:au_GetLatest {
+   # Discover the latest release version
+   $VersionURL  = 'https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html'
+   $download_page = Invoke-WebRequest -Uri $VersionURL #-UseBasicParsing -DisableKeepAlive
+   $ReleaseText = $download_page.links | 
+                     Where-Object {$_.innertext -match 'DC.*\([0-9.]+\)'} |
+                     Select-Object -ExpandProperty innertext -First 1
+   $Release = $ReleaseText -replace '.*\(([0-9.]+)\).*','$1'
+   $version = "20$Release"
+   $ReleaseFolder = $Release.replace('.','')
+
+   # Find the MSP for the latest version
+   $FTPbase = 'ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC'
+   $MSPpage = Invoke-WebRequest -Uri "$FTPbase/$ReleaseFolder" -UseBasicParsing -DisableKeepAlive
+   $MUIMSPstub = $MSPpage.rawcontent -split '"' |
+                  Where-Object {$_ -match 'MUI\.msp$'} |
+                  Select-Object -First 1
+   $MSPstub = $MSPpage.rawcontent -split '"' |
+                  Where-Object {$_ -match '\d\.msp$'} |
+                  Select-Object -First 1
+
+   # Find the most-recent EXE
+   $FTPpage = Invoke-WebRequest -Uri $FTPbase -UseBasicParsing -DisableKeepAlive
+   $Folders = $FTPpage.rawcontent -split '[\r\n]' | 
+                  Where-Object {$_ -match '>\d+<'} |
+                  ForEach-Object {$_ -replace '.*>(\d+)<.*','$1'} |
+                  Sort-Object -Descending
+   foreach ($folder in $Folders) {
+      $FolderFiles = Invoke-WebRequest -Uri "$FTPbase/$folder" -UseBasicParsing -DisableKeepAlive
+      $EXEstub = $FolderFiles.rawcontent -split '"' |
+                     Where-Object {$_ -match 'en_US\.exe$'} |
+                     Select-Object -First 1
+      if ($EXEstub) { break }
+   }
+   $MUIstub = $EXEstub -replace 'en_US','MUI'
+
+   return  @{ 
+       Version   = $version
+       URL32     = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/$MUIstub"
+       EXEurl    = "$FTPbase/$EXEstub"
+       MUIMSPurl = "$FTPbase/$MUIMSPstub"
+       MSPurl    = "$FTPbase/$MSPstub"
+   }
+}
 
 function global:au_SearchReplace {
     @{
@@ -13,56 +55,16 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $url -UseBasicParsing -DisableKeepAlive
-
-    $reLatestbuild = "(.*>DC.*)"
-    $download_page.RawContent -imatch $reLatestbuild
-    $latestbuild   = $Matches[0]
-
-    $reVersion   = "(\d+)(.)(\d+)(.)(\d+)"
-    $latestbuild -imatch $reVersion
-    $version     = "20$($Matches[0])"
-    $urlVersion  = $Matches[0].Replace(".", "")
-    
-    $urlPackageMSP = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/$($urlVersion)/AcroRdrDCUpd$($urlVersion)_MUI.msp"
-
-    return  @{ 
-        URL32          = $urlPackageMSP;
-        ChecksumType32 = $checksumTypePackageMSP;
-        Version        = $version
-    }
+function global:au_BeforeUpdate() {
+   $Latest.exeChecksum    = Get-RemoteChecksum $Latest.EXEurl
+   $Latest.MUIMSPChecksum = Get-RemoteChecksum $Latest.MUIMSPurl
+   $Latest.MSPChecksum    = Get-RemoteChecksum $Latest.MSPurl
 }
 
 function global:au_AfterUpdate ($Package) {
     Set-DescriptionFromReadme $Package -SkipFirst 3
 }
 
-update
+Update-Package
 
 
-
-
-
-$DownloadFolder = "E:\Adobe_Acrobat_Reader_DC_MUI\"
-$FTPFolderUrl = "ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/"
-
-#connect to ftp, and get directory listing
-$FTPRequest = [System.Net.FtpWebRequest]::Create("$FTPFolderUrl") 
-$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectory
-$FTPResponse = $FTPRequest.GetResponse()
-$ResponseStream = $FTPResponse.GetResponseStream()
-$FTPReader = New-Object System.IO.Streamreader -ArgumentList $ResponseStream
-$DirList = $FTPReader.ReadToEnd()
-
-#from Directory Listing get last entry in list, but skip one to avoid the 'misc' dir
-$LatestUpdate = $DirList -split '[\r\n]' | Where {$_} | Select -Last 1 -Skip 1
-
-#build file name
-$LatestFile = "AcroRdrDCUpd" + $LatestUpdate + "_MUI.msp"
-
-#build download url for latest file
-$DownloadURL = "$FTPFolderUrl$LatestUpdate/$LatestFile"
-
-#download file
-(New-Object System.Net.WebClient).DownloadFile($DownloadURL, "$DownloadFolder$LatestFile")
