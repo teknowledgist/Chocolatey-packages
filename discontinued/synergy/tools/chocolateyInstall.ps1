a36a8ad3c05e5bfe79341c700cafe82db8a15d47
teknowledgist <teknowledgist@users.noreply.github.com>
2019-01-07 09:16:56 -0500

    Create Synergy package

---
+++ discontinued/synergy/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   fileType       = 'msi'
+   File           = (Get-ChildItem $toolsDir -filter "*x86.msi").FullName
+   File64bit      = (Get-ChildItem $toolsDir -filter "*x64.msi").FullName
+   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+   validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
+
+
+
