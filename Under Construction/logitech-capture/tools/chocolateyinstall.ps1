73bc4f642e58b210ef189ea2e04291b4d791cdc5
teknowledgist <teknowledgist@users.noreply.github.com>
2019-02-01 17:08:58 -0500

    logitech attempt

---
+++ Under Construction/logitech-capture/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+   packageName   = $env:chocolateyPackageName
+   fileType      = 'exe'
+   url           = 'https://download01.logi.com/web/ftp/pub/techsupport/capture/Capture_1.0.553.exe'
+   checksum      = '4327ee44b6c03850ad35164505fd9a5f4cd20a1242bcd36ca03a21972f1ae687'
+   checksumType  = 'sha256'
+   silentArgs    = "/S"
+   validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs 
