e434eb7381da748cb0463bd4ea464735f70bd672
teknowledgist <teknowledgist@users.noreply.github.com>
2019-03-15 23:37:39 -0400

    create marcedit

---
+++ automatic/marcedit/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'  # stop on all errors
+
+$packageArgs = @{
+   packageName   = $env:chocolateyPackageName
+   fileType      = 'msi'
+   url           = 'https://marcedit.reeset.net/software/marcedit7/MarcEdit_Setup32Admin.msi'
+   url64         = 'https://marcedit.reeset.net/software/marcedit7/MarcEdit_Setup64Admin.msi'
+   checksum      = '9cfc28e3c34399ccedd0a72b7b80281a30c7651858c64f82fd2f7f7e623dc3ab'
+   checksum64    = '5b862efeb35403efc08d1d791aac89e8e648a5c0f8f8de881c91c0ff22a58e76'
+   checksumType  = 'sha256'
+   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+   validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs 
---
+++ automatic/marcedit/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$Release = 'https://marcedit.reeset.net/software/update7.txt'
+
+function global:au_GetLatest {
+   $download_page = Invoke-WebRequest -Uri "$Release"
+
+   $download_page -match "^7\.[0-9]+\.[0-9]+"
+   
+   $version = $Matches[0]
+
+   return @{ 
+      Version = $version
+      URL32 = 'https://marcedit.reeset.net/software/marcedit7/MarcEdit_Setup32Admin.msi'
+      URL64 = 'https://marcedit.reeset.net/software/marcedit7/MarcEdit_Setup64Admin.msi'
+   }
+}
+
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^\s*Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+Update-Package
