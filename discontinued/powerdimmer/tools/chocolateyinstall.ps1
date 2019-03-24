00c1ddeac01a03423fb491b7bb17e88c96ae3d7a
teknowledgist <teknowledgist@users.noreply.github.com>
2019-03-24 17:10:05 -0400

    added PowerDimmer

---
+++ discontinued/powerdimmer/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$ToolsDir   = Split-Path -parent $MyInvocation.MyCommand.Path
+
+$installArgs = @{
+   packageName  = $env:ChocolateyPackageName
+   FileFullPath = (Get-ChildItem $ToolsDir -Filter "*.zip").FullName
+   Destination  = Join-Path $env:SystemRoot 'system32'
+}
+
+Get-ChocolateyUnzip @installArgs
---
+++ discontinued/powerdimmer/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$File = Join-Path $env:SystemRoot 'system32\powerdimmer.scr'
+ 
+if ([System.IO.File]::Exists($File)) {
+    Write-Debug "Found the Power Dimmer screensaver file. Deleting it..."
+    [System.IO.File]::Delete($File)
+}
