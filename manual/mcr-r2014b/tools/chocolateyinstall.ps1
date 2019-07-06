e9d87694f8ef7e5e1043cf1c9e8448fbcd302b0f
Jean-Louis Jouannic <jean-louis.jouannic@zenika.com>
2019-07-06 19:16:48 +0200

    🐛 Fixes failing x86 MCR install

--- manual/mcr-r2014b/tools/chocolateyinstall.ps1
+++ manual/mcr-r2014b/tools/chocolateyinstall.ps1
@@ -1,21 +1,14 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'mcr-r2014b'
-$Version     = '8.4'
-$url         = 'https://www.mathworks.com/supportfiles/downloads/R2014b/deployment_files/R2014b/installers/win32/MCR_R2014b_win32_installer.exe'
-$url64       = 'https://www.mathworks.com/supportfiles/downloads/R2014b/deployment_files/R2014b/installers/win64/MCR_R2014b_win64_installer.exe'
-$checkSum    = 'C3575166396F7B3F427FF5A52B99E1AD6BAD046682F04FA7DA0B5F8D1073F52C'
-$checkSum64  = '6D43AB89D71374A8F864A91DD052753743202AF0BA1B13B3245D7A31813F8979'
-
-$WorkSpace = Join-Path $env:TEMP "$packageName.$Version"
+$WorkSpace = Join-Path $env:TEMP "$env:ChocolateyPackageName.$env:ChocolateyPackageVersion"
 
 $WebFileArgs = @{
-   packageName  = $packageName
+   packageName  = $env:ChocolateyPackageName
    FileFullPath = Join-Path $WorkSpace 'installer.exe'
-   Url          = $url
-   Url64bit     = $url64
-   Checksum     = $checkSum
-   Checksum64   = $checkSum64
+   Url          = 'https://www.mathworks.com/supportfiles/downloads/R2014b/deployment_files/R2014b/installers/win32/MCR_R2014b_win32_installer.exe'
+   Url64bit     = 'https://www.mathworks.com/supportfiles/downloads/R2014b/deployment_files/R2014b/installers/win64/MCR_R2014b_win64_installer.exe'
+   Checksum     = 'C3575166396F7B3F427FF5A52B99E1AD6BAD046682F04FA7DA0B5F8D1073F52C'
+   Checksum64   = '6D43AB89D71374A8F864A91DD052753743202AF0BA1B13B3245D7A31813F8979'
    ChecksumType = 'sha256'
    GetOriginalFileName = $true
 }
@@ -26,21 +19,20 @@ Write-Host $msgtext -ForegroundColor Cyan
 $PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
 
 $UnzipArgs = @{
-   PackageName = $packageName
+   PackageName = $env:ChocolateyPackageName
    FileFullPath = $PackedInstaller
    Destination  = $WorkSpace
 }
 
 Get-ChocolateyUnzip @UnzipArgs
 
-$Installer = (Get-ChildItem $WorkSpace -Include 'setup.exe' -Recurse).fullname
+$Installer = (Get-ChildItem -Path "$WorkSpace\bin" -Include 'setup.exe' -Recurse).fullname
 
 $InstallArgs = @{
-   PackageName = $packageName
+   PackageName = $env:ChocolateyPackageName
    File = $Installer
    SilentArgs = '-mode silent -agreeToLicense yes'
    UseOnlyPackageSilentArguments = $true
 }
 
 Install-ChocolateyInstallPackage @InstallArgs
-
