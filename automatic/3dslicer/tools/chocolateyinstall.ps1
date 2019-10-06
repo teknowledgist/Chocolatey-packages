b9dca744ce305b7acc8fb98483899efc43900dcb
teknowledgist <teknowledgist@users.noreply.github.com>
2019-10-05 23:57:22 -0400

    3dslicer

---
+++ automatic/3dslicer/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   softwareName   = 'Slicer*'
+   fileType       = 'EXE'
+   url64bit       = 'https://slicer.kitware.com/midas3/download?bitstream=1023243'
+   checksum64     = '5a459896883fba721ab27052d74c6ba7d49bd73b1b87c3a2f389a0db76955a50'
+   checksumType64 = 'sha256'
+   silentArgs     = '/S'
+   validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs 
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+New-Item (Join-path $toolsDir '.skipAutoUninstall') -Type file -Force | Out-Null
+
---
+++ automatic/3dslicer/update.ps1
@@ -0,0 +1,41 @@
+import-module au
+
+$Global:au_NoCheckUrl = $true
+
+function global:au_GetLatest {
+   $DownloadURI = 'https://download.slicer.org/'
+   $download_page = Invoke-WebRequest -Uri $DownloadURI
+
+   $TRow = $download_page.ParsedHtml.getElementsByTagName('tr') | 
+               Where-Object {$_.innertext -match 'stable release'} | 
+               Select-Object -ExpandProperty innerhtml
+   $TData = $TRow -split '</?T[DH]>' | Where-Object {$_ -match 'version'} | Select-Object -First 1
+
+   $HREF = $TData -replace '.*"/([^"]+)".*','$1'
+   $version = $TData -replace '.*version ([0-9.]+).*','$2'
+   $revision = $TData -replace '.*revision ([0-9.]+).*','$1'
+
+   $url64 = 'https://slicer.kitware.com/midas3/download?' + ($HREF -replace '/','=')
+
+   return @{ 
+            Version      = "$version.$revision"
+            NamedVersion = $version
+            URL64        = $url64
+         }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\chocolateyInstall.ps1" = @{
+         "(^   url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+         "(^   Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      }
+      "tools\chocolateyUninstall.ps1" = @{
+         "(^   softwarename\s*=\s*)('.*')"   = "`$1'Slicer $($Latest.NamedVersion)*'"
+      }
+   }
+}
+
+
+update -ChecksumFor 64
