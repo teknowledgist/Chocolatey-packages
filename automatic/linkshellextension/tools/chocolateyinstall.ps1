7f9d424de52a809ffb3fc312b1171335541a281c
teknowledgist <teknowledgist@users.noreply.github.com>
2019-11-17 22:19:49 -0500

    Update chocolateyinstall.ps1

--- automatic/linkshellextension/tools/chocolateyinstall.ps1
+++ automatic/linkshellextension/tools/chocolateyinstall.ps1
@@ -7,9 +7,9 @@ $InstallArgs = @{
    url            = 'http://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_win32.exe'
    url64bit       = 'http://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_X64.exe'
    checksumType   = 'sha256'
-   checksum       = '74d21fb9940df5af6d7a661cf6f306e9444a885f5bd367afce92847046214a6f'
-   checksum64     = 'f148bb3954b83b6ee269c17fa650f3a0248ba686239b1cff21ff17825b046e23'
-   silentArgs     = '/S'
+   checksum       = '2e62d4a7d4cdd4d36f67a82cd64d1187cb6411fdb5863c79faa09c7c751390c1'
+   checksum64     = 'dcaa5ef7c34846d1a41d6dc67d4887c86e2351c2a6561363e86e280a872d3f97'
+   silentArgs     = '/S /noredist'
    validExitCodes = @(0)
 }
 
