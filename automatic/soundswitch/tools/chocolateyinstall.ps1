87b40701e4821d99994bf613c1368e68eca542ab
teknowledgist <teknowledgist@users.noreply.github.com>
2020-01-08 21:22:17 -0500

    create soundswitch and update scp

---
+++ automatic/soundswitch/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop' 
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.exe').FullName
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileType       = 'exe'
+   File           = $fileLocation
+   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS /TASKS=`"`" /LOG=`"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).Install.log`""
+   validExitCodes = @(0)
+}
+
+Get-Process -Name soundswitch -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+
+Install-ChocolateyInstallPackage @InstallArgs
+New-Item "$fileLocation.ignore" -Type file -Force | Out-Null
---
+++ automatic/soundswitch/update.ps1
@@ -0,0 +1,44 @@
+import-module au
+
+function global:au_GetLatest {
+   $DownloadURI = 'https://github.com/Belphemur/SoundSwitch/releases/latest'
+   [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+   $download_page = Invoke-WebRequest -Uri $DownloadURI -UseBasicParsing
+
+   $urlstub = $download_page.rawcontent.split("<>") | 
+                Where-Object {$_ -match '\.exe"'} | Select-Object -First 1
+
+   $url = $urlstub -replace '.*?"([^ ]+\.exe).*','$1'
+
+   $version = $url.split('/') | Where-Object {$_ -match '^v[0-9.]+$'} |select -Last 1
+   $version = $version.trim('v')
+   
+   return @{ 
+            Version  = $version
+            URL64    = "https://github.com$url"
+            Options  = @{
+               Headers = @{
+                  ContentType = 'application/octet-stream'
+               }
+            }
+         }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\VERIFICATION.txt" = @{
+         "(^Version\s+:).*"      = "`${1} $($Latest.Version)"
+         "(^URL\s+:).*"          = "`${1} $($Latest.URL32)"
+         "(^Checksum\s+:).*"     = "`${1} $($Latest.Checksum)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Get-RemoteFiles -Purge -NoSuffix
+}
+
+
+update -ChecksumFor none
+if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
