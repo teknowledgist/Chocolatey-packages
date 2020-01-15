21159c7732dbcf3219d00349784220fc9437d8f7
Bob Tanner <tanner@real-time.com>
2020-01-15 16:16:50 -0600

    Closes: https://github.com/teknowledgist/Chocolatey-packages/issues/24
    draftsight 2019 SP3 update

--- manual/draftsight/tools/chocolateyinstall.ps1
+++ manual/draftsight/tools/chocolateyinstall.ps1
@@ -1,26 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'DraftSight'
-$url         = 'http://dl-ak.solidworks.com/nonsecure/draftsight/2019SP2/DraftSight32.exe'
-$url64       = 'http://dl-ak.solidworks.com/nonsecure/draftsight/2019SP2/DraftSight64.exe'
-$checkSum    = '7a632d6da849f05b721117225223a889da9937b383f5fdb1fbaabaa80e023a47'
-$checkSum64  = '08fab86549c9669c8e3610714f1a071fcef335c8b31352c0935080c84969b164'
+$url = 'http://dl-ak.solidworks.com/nonsecure/draftsight/2019SP3/DraftSight32.exe'
+$url64 = 'http://dl-ak.solidworks.com/nonsecure/draftsight/2019SP3/DraftSight64.exe'
+$checkSum = '39f1ec30b921fbbea8f6060612de3a073f8144b22701e3e2e4744bda3a5eb138'
+$checkSum64 = '31218d74987b200ffceab41b959e81a242345b2bd61222ca85a51b9c805d45c9'
 
 # if an older version of DraftSight has been run, the API service will prevent upgrading it.
-if (Get-Service -DisplayName "Draftsight API Service*" | Where {$_.status -eq 'running'}) {
+if (Get-Service -DisplayName "Draftsight API Service*" | Where { $_.status -eq 'running' }) {
    Stop-Service -DisplayName "Draftsight API Service*" -Force -PassThru
 }
 
 $WorkSpace = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
 
 $WebFileArgs = @{
-   packageName  = $packageName
-   FileFullPath = Join-Path $WorkSpace "$packageName.exe"
-   Url          = $url
-   Url64bit     = $url64
-   Checksum     = $checkSum
-   Checksum64   = $checkSum64
-   ChecksumType = 'sha256'
+   packageName         = $packageName
+   FileFullPath        = Join-Path $WorkSpace "$packageName.exe"
+   Url                 = $url
+   Url64bit            = $url64
+   Checksum            = $checkSum
+   Checksum64          = $checkSum64
+   ChecksumType        = 'sha256'
    GetOriginalFileName = $true
 }
 
