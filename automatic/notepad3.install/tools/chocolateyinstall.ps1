951bcbdd1b56d71a1bb0d2902e79a9789eb58a7b
teknowledgist <teknowledgist@users.noreply.github.com>
2020-05-13 16:23:12 -0400

    fixed Notepad3 URLs

--- automatic/notepad3.install/tools/chocolateyinstall.ps1
+++ automatic/notepad3.install/tools/chocolateyinstall.ps1
@@ -1,29 +1,55 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$BitLevel = Get-ProcessorBits
+
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$ZipFile = (Get-ChildItem $toolsDir -Filter '*.zip').FullName
-
-If ($ZipFile) {
+# This package is automated, but the embedded downloads are sometimes .zip and sometimes .exe
+$ZipFiles = Get-ChildItem $toolsDir -Filter '*.zip'
+If ($ZipFiles) {
    $UnzipDir = Join-Path $env:TEMP "$env:ChocolateyPackageName_$env:ChocolateyPackageVersion"
-   # Extract zip
-   Get-ChocolateyUnzip -FileFullPath $ZipFile -Destination $UnzipDir
+
+   If ($ZipFiles.count -eq 2) {
+      if ($BitLevel -eq '64') {
+         $ZipFile = $ZipFiles | Where-Object {$_.Name -notmatch 'x86'} 
+      } else {
+         $ZipFile = $ZipFiles | Where-Object {$_.Name -match 'x86'}
+   } else { $ZipFile = $ZipFiles }
+
+   Get-ChocolateyUnzip -FileFullPath $ZipFile.FullName -Destination $UnzipDir
+   }
 } else {
    $UnzipDir = $toolsDir
 }
 
-$Installer = (Get-ChildItem $UnzipDir -Filter '*.exe').FullName
-
-$packageArgs = @{
+$InstallArgs = @{
    packageName  = $env:ChocolateyPackageName
    fileType     = 'EXE' 
-   File         = $Installer
    softwareName = "$env:ChocolateyPackageName*"
    silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+$Installers = Get-ChildItem $UnzipDir -Filter '*.exe'
 
-if ($UnzipDir -eq $toolsDir) {
-   $null = New-Item "$Installer.ignore" -Type file -Force
+if ($installers.count -eq 2) {
+   # clearly, .zip not included
+   $Installer64 = $installers | Where-Object {$_.name -notmatch 'x86'} | Select-Object -ExpandProperty fullname
+   $Installer32 = $installers | Where-Object {$_.name -match 'x86'} | Select-Object -ExpandProperty fullname
+   $InstallArgs.add('File',$Installer32)
+   $InstallArgs.add('File64',$Installer64)
+} else {
+   # if zipped, then there should be only one .exe
+   if ($BitLevel -eq '64') {
+      $Installer64 = $installers | Where-Object {$_.name -notmatch 'x86'} | Select-Object -ExpandProperty fullname
+      $InstallArgs.add('File64',$Installer64)
+   } else {
+      $Installer32 = $installers | Select-Object -ExpandProperty fullname
+      $InstallArgs.add('File',$Installer32)
+   }
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
+
+foreach ($exe in $Installers) {
+   remove-item $exe.fullname -Force
 }
