f9e1de187c577938be253003803568d020bc6373
teknowledgist <teknowledgist@users.noreply.github.com>
2020-06-06 14:52:45 -0400

    speedyfox (manual) version update

--- automatic/speedyfox/tools/chocolateyinstall.ps1
+++ automatic/speedyfox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    unzipLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
    url           = 'https://www.crystalidea.com/downloads/speedyfox.zip'
-   checksum      = 'aee4ce9a7439676f511aa0997c6e96a0ebdd1bccbea1ca97dc864eeadfebfab0'
+   checksum      = '30B2C33D0E733E799D97AE47B26B4FE3A1AA11D4CAFCF8658BA228B8D8864E28'
    checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
