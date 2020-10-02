34568666f436030b6a23c68ad4ad3c62c5d8ddf8
teknowledgist <teknowledgist@users.noreply.github.com>
2020-10-02 13:26:51 -0400

    Adopt OneDrive

--- automatic/onedrive/tools/chocolateyinstall.ps1
+++ automatic/onedrive/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
    url           = 'https://go.microsoft.com/fwlink/?linkid=844652'
    checksum      = 'ed29bc59caa58162b135ff0d970df5651cb5802e3aef5fc02f7f8bedf9ca11b8'
    checksumType  = 'sha256'
-   silentArgs    = "/AllUsers"
+   silentArgs    = '/allusers /silent'
    validExitCodes= @(0)
 }
 
@@ -15,30 +15,32 @@ Install-ChocolateyPackage @packageArgs
 <# This is a machine-wide install.  At least some versions of 
    the installer do not remove the per-user install command
    to run the Win10 "built-in" OneDrive installer.
-   What follows should prevent that (as it is redundant).
+   What follows should prevent that redundant action.
    Reference:  https://byteben.com/bb/installing-the-onedrive-sync-client-in-per-machine-mode-during-your-task-sequence-for-a-lightening-fast-first-logon-experience/
 #> 
 
 try {
-   REG LOAD HKLM\DefaultUser "$env:SystemDrive\Users\Default\NTUSER.DAT"
-   $RegPath = 'HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run'
-   $ODrunValue = (Get-ItemProperty $RegPath).OneDriveSetup
-   if ($ODrunValue) {
-      Remove-ItemProperty -Path $RegPath -Name "OneDriveSetup" 
-      $RegText = @"
-Windows Registry Editor Version 5.00
+   $RegPath = "$env:windir\system32\reg.exe"
+   $KeyPath = 'HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run'
 
-; This key is not literal.  It was loaded from the default user registry file.
-[HKEY_LOCAL_MACHINE\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run]
-"OneDriveSetup"="$ODrunValue"
-"@
-      $RegText.replace("`n","`r`n") | out-file "$env:ChocolateyPackageFolder\Removed.reg" -Force
+   $null = Start-ChocolateyProcessAsAdmin -ExeToRun $RegPath -Statements "LOAD HKLM\DefaultUser `"$env:SystemDrive\Users\Default\NTUSER.DAT`""
+
+   $ODrunValue = (Get-ItemProperty -Path $KeyPath).OneDriveSetup
+   if ($ODrunValue) {
+      Remove-ItemProperty -Path $KeyPath -Name 'OneDriveSetup' -Force
+      $BackedUpKey = "The default user registry file was loaded and the value`r`n`r`n" +
+                     "OneDriveSetup = $ODrunValue`r`n`r`n" +
+                     "was removed from \Software\Microsoft\Windows\CurrentVersion\Run`r`n" +
+                     "Do not alter this file or the key  will not be restored on`r`n" +
+                     'uninstallation of this package.'
+      $BackedUpKey | out-file -FilePath "$env:ChocolateyPackageFolder\RemovedKeyInfo.txt" -Force
+      Write-Verbose 'Registry key information for default user install of OneDrive is backed up.'
    }
-   REG UNLOAD HKLM\DefaultUser
+   $null = Start-ChocolateyProcessAsAdmin -ExeToRun $RegPath -Statements 'UNLOAD HKLM\DefaultUser'
 } catch {
    $note = "Removal of default per-user install of OneDrive failed.`n" +
             "This should not affect OneDrive function but could slow`n" +
-            "down the login of new users."
+            'down the login of new users.'
    Write-Warning $note
 }
 
--- automatic/onedrive/tools/chocolateyuninstall.ps1
+++ automatic/onedrive/tools/chocolateyuninstall.ps1
@@ -1,17 +1,23 @@
 $ErrorActionPreference = 'Stop'
 
 # Restore the per-user install of the built-in OneDrive (if it was there)
-if (Test-Path "$env:ChocolateyPackageFolder\Removed.reg") {
-   REG LOAD HKLM\DefaultUser "$env:SystemDrive\Users\Default\NTUSER.DAT"
-   Start-sleep 1   # avoiding a race condition
-   REG IMPORT "$env:ChocolateyPackageFolder\Removed.reg"
-   Start-sleep 1   # avoiding a race condition
-   REG UNLOAD HKLM\DefaultUser
-   Write-Verbose "Previous registry key restored."
+if (Test-Path "$env:ChocolateyPackageFolder\RemovedKeyInfo.txt") {
+   $RegPath = "$env:windir\system32\reg.exe"
+   $KeyPath = 'HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run'
+
+   $null = Start-ChocolateyProcessAsAdmin -ExeToRun $RegPath -Statements "LOAD HKLM\DefaultUser `"$env:SystemDrive\Users\Default\NTUSER.DAT`""
+
+   $RegKey = (Get-Content "$env:ChocolateyPackageFolder\RemovedKeyInfo.txt" | 
+                  Where-Object {$_ -match '='}).split('=').trim()
+   Set-ItemProperty -Path $KeyPath -Name $RegKey[0] -Value $RegKey[1] -Force
+
+   $null = Start-ChocolateyProcessAsAdmin -ExeToRun $RegPath -Statements 'UNLOAD HKLM\DefaultUser'
+   Write-Verbose 'Previous registry key restored.'
 }
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Microsoft OneDrive*'
   fileType      = 'EXE'
   silentArgs    = '/uninstall /allusers'
   validExitCodes= @(0)
@@ -28,8 +34,8 @@ if ($key.Count -eq 1) {
   Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
+  Write-Warning 'To prevent accidental data loss, no programs will be uninstalled.'
+  Write-Warning 'Please alert package maintainer the following keys were matched:'
   $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
 
