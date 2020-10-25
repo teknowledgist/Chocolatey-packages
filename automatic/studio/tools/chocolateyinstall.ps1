9e1ab5ec13ebc956bb6d2a85e12055e6d8644a47
teknowledgist <teknowledgist@users.noreply.github.com>
2020-10-24 22:10:32 -0400

    3/4 of October 2020 updates

--- automatic/studio/tools/chocolateyinstall.ps1
+++ automatic/studio/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
    fileType      = 'EXE'
    url           = 'https://s3.amazonaws.com/blstudio/Studio2.0/Studio+2.0_32.exe'
    url64bit      = 'https://s3.amazonaws.com/blstudio/Studio2.0/Studio+2.0.exe'
-   checksum      = '740b2f15f228e34118446366d04babf7ad279e5dc4c0784a8fa3be9208591cf0'
-   checksum64    = 'bc908bc66f58320690aa3b5a95da7000aa8ede9f916dd6a540681d942569ced7'
+   checksum      = '84462dce8ee0f539e9a6de75a1153939eaf10915473fb5279c8a6c4ac3358c60'
+   checksum64    = '67681258e96c5d36eb16d4456b30df934486c4444cdd6216c760c3848c0aed14'
    checksumType  = 'sha256'
    silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).Install.log`""
    validExitCodes= @(0)
