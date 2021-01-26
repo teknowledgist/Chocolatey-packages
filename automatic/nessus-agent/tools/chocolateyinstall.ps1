d48ec4a674701395de21b2d2cc5a265a0f466bcd
teknowledgist <teknowledgist@users.noreply.github.com>
2021-01-26 13:25:33 -0500

    upload nessus-agent files

---
+++ automatic/nessus-agent/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+   packageName   = $env:ChocolateyPackageName
+   fileType      = 'MSI'
+   url           = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/12160/download?i_agree_to_tenable_license_agreement=true'
+   url64bit      = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/12159/download?i_agree_to_tenable_license_agreement=true'
+   softwareName  = 'Nessus Agent*'
+   checksum      = 'f53d7deefd661476be57092b28accc7412ce792b7fa84e6b2fdd58a4907624d7'
+   checksum64    = '4c111c11de5230af57b9de310fdd91f13555f33a4b0afdb96378e1ac774ef058'
+   checksumType  = 'sha256' 
+   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($Env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`" "
+   validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/nessus-agent/update.ps1
@@ -0,0 +1,38 @@
+import-module au
+
+function global:au_GetLatest {
+   $ReleasURL = 'https://www.tenable.com/downloads/nessus-agents'
+   [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+   $Release_page = Invoke-WebRequest -Uri $ReleasURL
+
+   $null = $Release_page.links |
+               Where-Object {$_.innertext -match 'Nessus Agents ([0-9.]+)'} | 
+               Select-Object -first 1
+
+   $version = $Matches[1]
+
+   $metadata = $Release_page.content.split('{}') | ? {$_ -match $version -and $_ -match 'msi"'} | select -First 2
+
+   $x64ID = ($metadata | ? {$_ -match 'x64'}) -replace '.*"id":([0-9]+),.*','$1'
+   $x32ID = ($metadata | ? {$_ -match 'Win32'}) -replace '.*"id":([0-9]+),.*','$1'
+
+   return @{ 
+      Version = $version
+      URL32 = "https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/$x32ID/download?i_agree_to_tenable_license_agreement=true"
+      URL64 = "https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/$x64ID/download?i_agree_to_tenable_license_agreement=true"
+   }
+}
+
+function global:au_SearchReplace {
+   @{
+      "tools\chocolateyInstall.ps1" = @{
+         "(^   url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+         "(^   url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+         "(^   Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+         "(^   Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      }
+   }
+}
+
+Update-Package -ChecksumFor all
