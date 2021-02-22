245de2dd3c91bf2b690e595a4ab892a29bfb2569
teknowledgist <teknowledgist@users.noreply.github.com>
2021-02-22 12:52:26 -0500

    Ocularis update options

--- Local/ocularis-client/update.ps1
+++ Local/ocularis-client/update.ps1
@@ -1,19 +1,32 @@
 import-module au
 
 $LocalDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
-$HostName = Get-Content "$LocalDir\HostName.private"
+if (Test-Path "$LocalDir\HostName.private") {
+   $HostName = Get-Content "$LocalDir\HostName.private"
 
-# View:  http://$HostName/ocularisservice/InstallerWebsite/client.html
-$Release = "http://$HostName/ocularisservice/InstallerWebsite"
+   # View:  http://$HostName/ocularisservice/InstallerWebsite/client.html
+   $extra = '/client.html'
+   $Release = "http://$HostName/ocularisservice/InstallerWebsite"
+}
+if (-not $HostName) {
+   $Release = 'https://www.qognify.com/support-training/software-downloads/'
+   $extra = ''
+}
+
 
 function global:au_GetLatest {
    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-   $download_page = Invoke-WebRequest -Uri "$Release/client.html"
+   $download_page = Invoke-WebRequest -Uri "$Release$extra"
 
    $urlstub = $download_page.links | 
-               Where-Object {$_.href -match 'client'} | 
-               Select-Object -ExpandProperty href
-   $url = "$Release/$urlstub" -replace ' ','%20'
+               Where-Object {$_.href -match 'ocularis.*client.*\.exe'} | 
+               Select-Object -ExpandProperty href -First 1
+   if ($urlstub -match '^http') {
+      $url = $urlstub
+   } else {
+      $url = "$Release/$urlstub" -replace ' ','%20'
+   }
+   $url = $url -replace ' ','%20'
 
    $version = $download_page.RawContent.split('> <') | Where-Object {$_ -match 'v[0-9.]+'}
    $version = $version.trim('v')
