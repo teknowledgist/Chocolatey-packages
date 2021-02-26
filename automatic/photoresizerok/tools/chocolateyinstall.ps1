9f1e8852692ac1da83018e3191c6ad0918dac105
teknowledgist <teknowledgist@users.noreply.github.com>
2021-02-26 08:25:27 -0500

    testing photoresizer

---
+++ automatic/photoresizerok/tools/chocolateyinstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Remove previous versions
+$Previous = Get-ChildItem $env:ChocolateyPackageFolder -filter "*.exe" 
+if ($Previous) {
+   $Previous | % { Remove-Item $_.FullName -Recurse -Force }
+}
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$BitLevel = Get-ProcessorBits
+
+$ZipFiles = (Get-ChildItem -Path $toolsDir -filter '*.zip' |
+                        Sort-Object lastwritetime | Select-Object -Last 2).FullName
+if ($BitLevel -eq '64') {
+   $ZipFile = $ZipFiles | Where-Object {$_.Name -match 'x64'} 
+} else {
+   $ZipFile = $ZipFiles | Where-Object {$_.Name -notmatch 'x64'}
+}
+
+$packageArgs = @{
+   packageName  = $env:ChocolateyPackageName
+   FileFullPath = $ZipFile
+   Destination  = $env:ChocolateyPackageFolder
+}
+Get-ChocolateyUnzip @packageArgs
+
+$GUI = (Get-ChildItem $env:ChocolateyPackageFolder -filter *.exe).fullname
+$null = New-Item "$GUI.gui" -Type file -Force
+
+$StartPrograms = Join-Path $env:ProgramData '\Microsoft\Windows\Start Menu\Programs'
+
+$ShortcutArgs = @{
+   ShortcutFilePath = Join-Path $StartPrograms "PhotoResizerOK.lnk"
+   TargetPath = $GUI
+}
+Install-ChocolateyShortcut @ShortcutArgs
---
+++ automatic/photoresizerok/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+function global:au_GetLatest {
+   $HomeURL = 'https://www.softwareok.com/?Download=PhotoResizerOK'
+   $PageText = Invoke-WebRequest -Uri $HomeURL -UseBasicParsing
+
+   $Title = ($PageText.rawcontent -split "title>|</title")[1]
+   $Version = $title -replace '.* ([0-9.]+) .*','$1'
+
+   return @{ 
+            Version  = $Version
+            URL32    = 'https://www.softwareok.com/Download/PhotoResizerOK.zip'
+            URL64    = 'https://www.softwareok.com/Download/PhotoResizerOK_x64.zip'
+           }
+}
+
+function global:au_SearchReplace {
+   @{
+      "tools\VERIFICATION.txt" = @{
+         "(^Version\s+:).*"      = "`${1} $($Latest.Version)"
+         "(^x86 URL\s+:).*"          = "`${1} $($Latest.URL32)"
+         "(^x86 Checksum\s+:).*"     = "`${1} $($Latest.Checksum32)"
+         "(^x64 URL\s+:).*"          = "`${1} $($Latest.URL64)"
+         "(^x64 Checksum\s+:).*"     = "`${1} $($Latest.Checksum364)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading PhotoResizerOK v$($Latest.Version) zip files"
+   Get-RemoteFiles -Purge -NoSuffix
+}
+
+update -ChecksumFor none -nocheckchocoversion
