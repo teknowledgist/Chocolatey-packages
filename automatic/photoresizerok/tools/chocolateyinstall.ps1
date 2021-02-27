14a7f95d3c43e9e766ceb272c73c15b5243043cf
teknowledgist <teknowledgist@users.noreply.github.com>
2021-02-26 20:40:57 -0500

    create photoresizerok package

--- automatic/photoresizerok/tools/chocolateyinstall.ps1
+++ automatic/photoresizerok/tools/chocolateyinstall.ps1
@@ -3,26 +3,27 @@
 # Remove previous versions
 $Previous = Get-ChildItem $env:ChocolateyPackageFolder -filter "*.exe" 
 if ($Previous) {
-   $Previous | % { Remove-Item $_.FullName -Recurse -Force }
+   $Previous | % { Remove-Item $_.FullName -Force }
 }
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $BitLevel = Get-ProcessorBits
 
-$ZipFiles = (Get-ChildItem -Path $toolsDir -filter '*.zip' |
-                        Sort-Object lastwritetime | Select-Object -Last 2).FullName
+$ZipFiles = Get-ChildItem -Path $toolsDir -filter '*.zip' |
+                  Sort-Object lastwritetime | Select-Object -Last 2 -ExpandProperty FullName
+
 if ($BitLevel -eq '64') {
-   $ZipFile = $ZipFiles | Where-Object {$_.Name -match 'x64'} 
+   $ZipFile = $ZipFiles | Where-Object {$_ -match 'x64'} 
 } else {
-   $ZipFile = $ZipFiles | Where-Object {$_.Name -notmatch 'x64'}
+   $ZipFile = $ZipFiles | Where-Object {$_ -notmatch 'x64'}
 }
 
-$packageArgs = @{
+$UnZipArgs = @{
    packageName  = $env:ChocolateyPackageName
    FileFullPath = $ZipFile
    Destination  = $env:ChocolateyPackageFolder
 }
-Get-ChocolateyUnzip @packageArgs
+Get-ChocolateyUnzip @UnZipArgs
 
 $GUI = (Get-ChildItem $env:ChocolateyPackageFolder -filter *.exe).fullname
 $null = New-Item "$GUI.gui" -Type file -Force
