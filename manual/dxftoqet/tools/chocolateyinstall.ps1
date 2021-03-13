8459d0267fe3ecf6d7ce6e5156224c15a34db17c
teknowledgist <teknowledgist@users.noreply.github.com>
2021-03-12 21:50:04 -0500

    create dxftoqet package

---
+++ manual/dxftoqet/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+if (Get-ProcessorBits -ne 64) {
+   Throw "$env:ChocolateyPackageName only runs on 64-bit systems."
+}
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$EXE = Get-ChildItem $toolsDir -Filter '*.exe' |
+               Sort-Object LastWriteTime | 
+               Select-Object -Last 1
+
+# Remove old versions
+Get-ChildItem $toolsDir -Filter '*.exe' | 
+      Where-Object {$_ -ne $EXE} | 
+      ForEach-Object { Remove-Item $_.fullname }
+
