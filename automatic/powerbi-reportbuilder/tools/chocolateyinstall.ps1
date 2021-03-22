1c6bb40dd8c21bc9ebfd56770af71475d03ad3b5
teknowledgist <teknowledgist@users.noreply.github.com>
2021-03-21 22:50:16 -0400

    make PowerBi-ReportBuilder multilingual

--- automatic/powerbi-reportbuilder/tools/chocolateyinstall.ps1
+++ automatic/powerbi-reportbuilder/tools/chocolateyinstall.ps1
@@ -10,4 +10,21 @@ $packageArgs = @{
    validExitCodes = @(0, 3010, 1641)
 }
 
+$pp = Get-PackageParameters
+
+if ($pp['Language']) { 
+   Write-Host "Language code '$($pp['Language'])' requested." -ForegroundColor Cyan
+   $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+   $Lang = Import-Csv "$toolsDir\LanguageChecksums.csv" -Delimiter '|' | 
+               Where-Object {$_.Code -eq $pp['Language']}
+   if ($Lang.URL -and $Lang.SHA256) {
+      Write-Host "$($Lang.Name) download url and checksum identified." -ForegroundColor Cyan
+      $packageArgs.url = $Lang.URL
+      $packageArgs.checksum = $Lang.SHA256
+   } else {
+      Write-Warning "Dowload URL and/or checksum for '$($pp['Language'])' not found!"
+      Write-Warning "Default English (US) language will be installed."
+   }
+} 
+
 Install-ChocolateyPackage @packageArgs
--- automatic/powerbi-reportbuilder/update.ps1
+++ automatic/powerbi-reportbuilder/update.ps1
@@ -3,34 +3,68 @@ Import-module au
 function global:au_GetLatest {
    $MainURL = 'https://www.microsoft.com/en-us/download/details.aspx?id=58158'
    $MainPage = Invoke-WebRequest -Uri $MainURL
-   $select = $MainPage.RawContent -split "select[^e]" | ? {$_ -match 'newlocale'}
-   $list = $select -split '</?option' | ? {$_ -match '^ ?value'}
-   $Language = $list -replace ' ?value=','* ' -replace '"','**' -replace '>',' --> '
 
-   $version = $MainPage.RawContent -split "</?p>" | ? {$_ -match '^[0-9.]+$'}
+   $version = $MainPage.RawContent -split '</?p>' | Where-Object {$_ -match '^[0-9.]+$'}
 
-   $ID = ($MainPage.rawcontent.split('"') |? {$_ -match 'confirmation'} |select -first 1) -replace '.*id=(\d+).*','$1'
-   $UID = ($MainPage.rawcontent.split('"') |? {$_ -match "$ID&"} |select -first 1) -replace '.*amp;([0-9a-f-]+).*','$1'
+   $ID = ($MainPage.rawcontent.split('"') |
+                     Where-Object {$_ -match 'confirmation'} |
+                     Select-Object -first 1) -replace '.*id=(\d+).*','$1'
+   $UID = ($MainPage.rawcontent.split('"') |
+                  Where-Object {$_ -match "$ID&"} |
+                  Select-Object -first 1) -replace '.*amp;([0-9a-f-]+).*','$1'
 
    $confirmURL = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=$ID&amp;$UID=1"
 
    $confirmpage = Invoke-WebRequest $confirmURL -UseBasicParsing
 
-   $URL32 = $confirmpage.rawcontent.split('"') | ? {$_ -match '\.msi$'} | select -first 1
+   $URL32 = $confirmpage.rawcontent.split('"') | 
+                  Where-Object {$_ -match '\.msi$'} | Select-Object -first 1
 
-   return @{ 
-            Version  = $Version
-            URL32    = $URL32
-           }
+   # Gather other languages download info
+   $CSV = @('Code|Language|URL|SHA256')
+   $select = $MainPage.RawContent -split 'select[^e]' | Where-Object {$_ -match 'newlocale'}
+   $list = $select -split '</?option' | Where-Object {$_ -match '^ ?value'}
+   $Languages = $list -replace ' ?value=','' -replace '^"','' -replace '">','|'
+   foreach ($lang in $Languages) {
+      $confirmURL = "https://www.microsoft.com/$($Lang.split('|')[0])/download/confirmation.aspx?id=$ID&amp;$UID=1"
+      $confirmpage = Invoke-WebRequest $confirmURL -UseBasicParsing
+      $LangURL = $confirmpage.rawcontent.split('"') | Where-Object {$_ -match '\.msi$'} | Select-Object -first 1
+      $CSV += "$Lang|$LangURL"
+   }
+   $CSV | Out-File '.\tools\LanguageChecksums.csv' -Force
+
+   $LatestHT = @{
+      Version  = $Version
+      URL32    = $URL32
+   }
+   foreach ($line in $CSV) {
+      $LangCode = $line.split('|')[0]
+      $LangDL = $line.split('|')[-1]
+      $LatestHT.Add($LangCode,$LangDL)
+   }
+   return $LatestHT
 }
 
 function global:au_SearchReplace {
+   $LangHT = @{}
+   $LangList = @{}
+   $Latest.GetEnumerator().Name | ? {$_ -match '^..-..$' } | % {
+      $LangHT.add("(^$($_)\|.*?\|).*","`$1$($Latest.$_)|" + $Latest."$($_)checksum")
+   }
    @{
-      "tools\chocolateyInstall.ps1" = @{
+      'tools\chocolateyInstall.ps1' = @{
          "(^   url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
          "(^   Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       }
+      'tools\LanguageChecksums.csv' = $LangHT
    }
 }
 
-Update-Package -ChecksumFor all
+function global:au_BeforeUpdate() {
+   $Latest.GetEnumerator().Name | ? {$_ -match '^..-..$' } | % {
+      Write-Host "Downloading $_ language version."
+      $Latest."$($_)checksum" = Get-RemoteChecksum $Latest.$_
+   }
+}
+
+Update-Package -ChecksumFor all -Force
