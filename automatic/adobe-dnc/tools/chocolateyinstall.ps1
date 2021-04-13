8e9b177bfea4371bd3144f52e520c683fc8587c1
teknowledgist <teknowledgist@users.noreply.github.com>
2021-04-12 22:51:22 -0400

    limit adobe-dnc to newer Win10

--- automatic/adobe-dnc/tools/chocolateyinstall.ps1
+++ automatic/adobe-dnc/tools/chocolateyinstall.ps1
@@ -1,5 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+Write-Warning "Adobe DNG Converter requires Windows 10, release 1809 or newer."
+$1809Build = 17763
+$osInfo = Get-WmiObject Win32_OperatingSystem | Select-Object Version, BuildNumber, Caption, ReleaseID
+$osInfo.Version = [version]$osInfo.Version
+$osInfo.ReleaseID = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseID).ReleaseID
+
+Write-host "Detected:  $($osInfo.Caption), Release $($osInfo.ReleaseID)" -ForegroundColor Cyan
+
+If (($osInfo.Version.Major -ne 10) -or ($osInfo.BuildNumber -lt $1809Build) -or ($osInfo.Caption -match 'ltsc')) {
+   Throw "$($osInfo.Caption), Release $($osInfo.ReleaseID) is not compatible with Adobe DNG Converter!"
+}
+
 $packageArgs = @{
    packageName    = $env:ChocolateyPackageName
    softwareName   = 'DNGConverter*'
