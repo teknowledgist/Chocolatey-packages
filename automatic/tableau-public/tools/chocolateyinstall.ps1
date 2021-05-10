648b77e199fc352deb4ab0a289aef6edbab5d05f
teknowledgist <teknowledgist@users.noreply.github.com>
2021-05-10 09:27:40 -0400

    Tweaks for tableau-public

--- automatic/tableau-public/tools/chocolateyinstall.ps1
+++ automatic/tableau-public/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
    checksum64    = 'acbdec4ce224fb2a5a893346f59d7a32eb0365f6ed6d6c88658046c5d2030fb2'
    checksumType  = 'sha256'
    silentArgs    = "/quiet /norestart /LOG `"$($env:TEMP)\TableauPublic-$($env:chocolateyPackageVersion)-InstallLogs\Install.log`" ACCEPTEULA=1"
-   validExitCodes= @(0)
+   validExitCodes= @(0,3010)
 }
 
 Install-ChocolateyPackage @packageArgs
