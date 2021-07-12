a3c8e51a077a5d9edfd763501404fb62a7f1cb0e
teknowledgist <teknowledgist@users.noreply.github.com>
2021-07-11 23:31:45 -0400

    update tableau-public 2021.2.0

--- automatic/tableau-public/tools/chocolateyinstall.ps1
+++ automatic/tableau-public/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    fileType      = 'EXE'
-   url64bit      = 'https://downloads.tableau.com/public/TableauPublicDesktop-64bit-2021-1-2.exe'
-   checksum64    = 'b05468d697d458bad5fe05c9a45bf832ec4b8f948e7677671e966a577c923974'
+   url64bit      = 'https://downloads.tableau.com/public/TableauPublicDesktop-64bit-2021-2-0.exe'
+   checksum64    = '4EC31A159249112AF902D2132CFADF701DF98DE9A5448BFEECE64C33787C54F4'
    checksumType  = 'sha256'
    silentArgs    = "/quiet /norestart /LOG `"$($env:TEMP)\TableauPublic-$($env:chocolateyPackageVersion)-InstallLogs\Install.log`" ACCEPTEULA=1"
    validExitCodes= @(0,3010)
