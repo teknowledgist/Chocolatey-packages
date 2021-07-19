228fadf0a0d3fbb14aed6ff0836cf6bd75c81a1a
teknowledgist <teknowledgist@users.noreply.github.com>
2021-07-19 14:25:27 -0400

    create ksp-client

---
+++ automatic/ksp-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$BitLevel = Get-ProcessorBits
+
+$pp = Get-PackageParameters
+
+if (!$pp['Host']) {
+   Write-Warning 'The KSP client is useless without a host!  You will have to run KeyAccess to add it later.'
+} else {
+   Write-Host "You have requested to use KeyServer: $($pp['Host'])" -ForegroundColor Cyan
+   $HostSwitch = "-v PROP_HOSTNAME=$($pp['Host'])" 
+}
+
+if (!$pp['Source']) {
+   $DownloadServer = 'https://www.sassafras.com/software/release/current/Installers/Windows/client'
+} else {
+   $CondensedVersion = '-' + $env:ChocolateyPackageVersion.replace('.','')
+   $DownloadServer = "https://$($pp['Source'])/kami"
+}
+
+$InstallArgs = @{
+   packageName   = $env:ChocolateyPackageName
+   softwareName  = 'Sassafras Keyserver Platform Client*'
+   fileType      = 'EXE'
+   url           = "$DownloadServer/ksp-client-i386$CondensedVersion.exe"
+   url64bit      = "$DownloadServer/ksp-client-x64$CondensedVersion.exe"
+   checksum      = '5bfa7a382d334534a3491c4bb192f9f6e7f23853907c5196d1adfb5a7a5a4b38'
+   checksum64    = '63c69106cd956979b17440a1c1d2e3ef813234624a647e3293918c932f522fa0'
+   checksumType  = 'sha256'
+   silentArgs    = "-q -platform $BitLevel -upg -v $HostSwitch PROP_REBOOT=0 -v PROP_SHORTCUTS=0"
+   validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
---
+++ automatic/ksp-client/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+function global:au_GetLatest {
+   $DUrl = 'https://www.sassafras.com/client-download/'
+   $download_page = Invoke-WebRequest -Uri "$DUrl"
+
+   $null = $download_page.AllElements | 
+         Where-Object {$_.tagname -eq 'h3' -and $_.innertext -match '([0-9.]+) for Windows'}
+   
+   return @{ 
+      Version = $Matches[1]
+   }
+}
+
+function global:au_SearchReplace {
+    @{
+       "tools\chocolateyInstall.ps1" = @{
+          "(^\s*Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+          "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+       }
+    }
+}
+
+Update-Package
