38bb392ffb7073ad87c5c9f43c6273a2d2257cf4
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-06 21:43:32 -0400

    update and move mpw(.portable) to discontinued

--- discontinued/mpw.portable/tools/chocolateyInstall.ps1
+++ discontinued/mpw.portable/tools/chocolateyInstall.ps1
@@ -2,12 +2,14 @@
 
 $ToolsDir   = Split-Path -parent $MyInvocation.MyCommand.Path
 
-$target = (Get-ChildItem $ToolsDir -filter *.jar -Recurse).fullname
+$target = Get-ChildItem $ToolsDir -filter *.jar |
+               Sort-Object LastWriteTime | 
+               Select-Object -ExpandProperty FullName -Last 1
 
-$JReg = 'HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment'
-$JVer=(Get-ItemProperty $JReg).CurrentVersion
-$JavaHome = (Get-ItemProperty $JReg/$JVer).JavaHome
-$JavaPath = (Get-ChildItem $JavaHome -Filter 'javaw.exe' -Recurse).fullname
+# If the Java dependency was just installed, the environment
+#   must be updated to access the 'JAVA_HOME' variable
+Update-SessionEnvironment
+$JavaPath = (Get-ChildItem $env:Java_Home -Filter 'javaw.exe' -Recurse).fullname
 
 $StartPrograms = Join-Path $env:ProgramData '\Microsoft\Windows\Start Menu\Programs'
 
