fc15c63b9342e5cef1b700ace6bfaecbb79e29a7
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-14 17:20:29 -0400

    creating oswald.font

---
+++ manual/oswald.font/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$Installed = Add-Font "$toolsDir\*.otf" -Multiple
+
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} else {
+   Write-Host "$Installed fonts were installed."
+}
+
+Remove-Item "$toolsDir\*.otf"
