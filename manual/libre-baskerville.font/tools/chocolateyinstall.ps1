028f7e867c0821f46f9b3c9a495e0f289204cb84
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-15 12:19:51 -0400

    create libre-baskerville.font package

---
+++ manual/libre-baskerville.font/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.zip').FullName
+$UnzipDir = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+$UnzipArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = $fileLocation
+   Destination    = $UnzipDir
+}
+Get-ChocolateyUnzip @UnzipArgs
+
+$FontFiles = Get-ChildItem $UnzipDir -Include "*.ttf" -Recurse | 
+                  Select-Object -ExpandProperty FullName
+
+$Installed = Add-Font $FontFiles -Multiple
+
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} elseif ($Installed -lt $FontFiles.count) {
+   Write-Host "$Installed fonts were installed." -ForegroundColor Cyan
+   Write-Warning "$($FontFiles.count - $Installed) submitted font paths failed to install."
+} else {
+   Write-Host "$Installed fonts were installed."
+}
+
+Remove-Item $fileLocation -Force
