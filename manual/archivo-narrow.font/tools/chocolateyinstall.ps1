ad5dbc04153073aad038535bc4e1ef40aab63f3a
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-15 11:35:16 -0400

    create archivo-narrow.font

---
+++ manual/archivo-narrow.font/tools/chocolateyinstall.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.zip').FullName
+$UnzipDir = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+$UnzipArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = $fileLocation
+   Destination    = $UnzipDir
+}
+Get-ChocolateyUnzip @UnzipArgs
+
+$pp = Get-PackageParameters
+
+if ($pp.contains('TTF')) { $EXT = 'TTF' }
+else { $EXT = 'OTF' }
+
+$FontFiles = Get-ChildItem $UnzipDir -Include "*.$EXT" -Recurse | 
+                  Select-Object -ExpandProperty FullName
+
+$Installed = Add-Font $FontFiles -Multiple
+
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} elseif ($Installed -lt $FontFiles.count) {
+   Write-Host "$Installed fonts were installed." -ForegroundColor Cyan
+   Write-Warning "$($FontFiles.count - $Installed) submitted font paths failed to install."
+} else {
+   Write-Host "$Installed fonts were installed."
+}
+
+Remove-Item $fileLocation -Force
