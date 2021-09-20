7baefe9026815799b3b62f38b6d827d91c1e764f
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-20 10:56:35 -0400

    Create final markdown-edit version

---
+++ discontinued/markdown-edit/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$OSversion = [version](Get-WmiObject -Class Win32_OperatingSystem).version 
+if ($OSversion -eq $null -or $OSversion -lt [Version]'6.2') {
+  Write-Host "This version of Markdown-Edit is designed for use with fonts that do not come with this version of Windows." -ForegroundColor Cyan
+}
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$Installer = Get-ChildItem $toolsDir -filter '*.msi' | Sort-Object LastWriteTime | Select-Object -Last 1
+
+$packageArgs = @{
+   packageName   = $env:chocolateyPackageName
+   fileType      = 'msi'
+   File64         = $Installer.fullname
+   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+   validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs 
+
+remove-item $Installer.fullname -Force
