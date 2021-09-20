9f67ce519488e9991d972fbd52ea2f9c8ce3930f
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-20 14:05:31 -0400

    create public-sans.font package

---
+++ automatic/public-sans.font/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.zip').FullName
+$UnzipDir = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+$UnzipArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = $fileLocation
+   Destination    = $UnzipDir
+}
+Get-ChocolateyUnzip @UnzipArgs
+
+$pp = Get-PackageParameters
+
+$FontFiles = Get-ChildItem $UnzipDir -Include "*.OTF" -Recurse | 
+                  Select-Object -ExpandProperty FullName
+
+$Installed = Add-Font $FontFiles -Multiple
+
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} elseif ($Installed -lt $FontFiles.count) {
+   Write-Host "$Installed fonts were installed." -ForegroundColor Cyan
+   Write-Warning "$($FontFiles.count - $Installed) submitted font paths failed to install."
+} else {
+   Write-Host "$Installed fonts were installed."
+}
+
+Remove-Item $fileLocation -Force
---
+++ automatic/public-sans.font/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+$Release = 'https://github.com/uswds/public-sans/releases/latest'
+
+function global:au_GetLatest {
+   [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+   $download_page = Invoke-WebRequest -Uri $Release -UseBasicParsing
+
+   $urlstring = $download_page.rawcontent.split(' ') | 
+                  Where-Object {$_ -match '\.zip'} | Select-Object -First 1
+   $urlstub = $urlstring.trim('"').split('"')[-1]
+
+   $version = $urlstub.split('/').trim('v') | Where-Object {$_ -match '^[0-9.]+$'}
+
+   return @{ 
+      Version = $version
+      URL32     = "https://github.com$urlstub"
+   }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\VERIFICATION.txt" = @{
+            "(^Version\s+:).*" = "`${1} $($Latest.Version)"
+            "(^URL\s+:).*"     = "`${1} $($Latest.URL32)"
+            "(^SHA256\s+:).*"  = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading Public Sans font $($Latest.Version) zip file"
+   Get-RemoteFiles -Purge -nosuffix
+}
+
+update -ChecksumFor none
