c00f06646a78c7b9e8de6f915ff0fc0dae472124
teknowledgist <teknowledgist@users.noreply.github.com>
2021-09-20 10:56:59 -0400

    create roboto.font package

---
+++ automatic/roboto.font/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.zip').FullName
+$UnzipDir = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+$UnzipArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = $fileLocation
+   Destination    = $UnzipDir
+}
+Get-ChocolateyUnzip @UnzipArgs
+
+$pp = Get-PackageParameters
+
+$FontFiles = Get-ChildItem $UnzipDir -Include "*.TTF" -Recurse | 
+                  Select-Object -ExpandProperty FullName
+
+if ($pp.contains('Condensed')) {
+   if (-not $pp.contains('Normal')) {
+      $FontFiles = $FontFiles | Where-Object {$_ -match 'Condensed'}
+   }
+} elseif ($pp.contains('Normal')) {
+   $FontFiles = $FontFiles | Where-Object {$_ -notmatch 'Condensed'}
+}
+
+$Installed = Add-Font $FontFiles -Multiple
+
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} elseif ($Installed -lt $FontFiles.count) {
+   Write-Host "$Installed fonts were installed." -ForegroundColor Cyan
+   Write-Warning "$($FontFiles.count - $Installed) submitted font paths failed to install."
+} else {
+   Write-Host "$Installed fonts were installed."
+}
+
+Remove-Item $fileLocation -Force
