0fee9798e6b213f1315cd440f07d500a2491ee63
teknowledgist <teknowledgist@users.noreply.github.com>
2021-10-14 13:34:54 -0400

    create key-n-stroke package

---
+++ manual/key-n-stroke/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$GUI = (Get-ChildItem $toolsDir -filter *.exe).fullname
+$null = New-Item "$GUI.gui" -Type file -Force
+
+$StartPrograms = Join-Path $env:ProgramData '\Microsoft\Windows\Start Menu\Programs'
+
+$ShortcutArgs = @{
+   ShortcutFilePath = Join-Path $StartPrograms "Key-n-Stroke.lnk"
+   TargetPath = $GUI
+   IconLocation = (Get-ChildItem $toolsDir -filter *.ico).FullName
+}
+Install-ChocolateyShortcut @ShortcutArgs
---
+++ manual/key-n-stroke/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$StartShortcut = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Key-n-Stroke.lnk'
+
+if(Test-Path $StartShortcut) {
+   Remove-Item $StartShortcut -Force
+}
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+# Self-updates get left behind, so let's clean it up a bit.
+Get-ChildItem $toolsDir -filter *.exe | ForEach-Object {$null = Remove-item $_.FullName -Force}
