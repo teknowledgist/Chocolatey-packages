40f39c18abd0d0d46e93ca706807d3e73d15adfe
teknowledgist <teknowledgist@users.noreply.github.com>
2022-03-24 11:11:26 -0400

    Remove hash check from psexec

--- automatic/psexec/tools/chocolateyinstall.ps1
+++ automatic/psexec/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $Url      = 'https://download.sysinternals.com/files/PSTools.zip'
-$Checksum = 'fc9a9d961eb4e9f069d53d29c7c3ef7d00b11754807b528c43a44e9e57e1cfae'
 
 # Remove old versions
 $null = Get-ChildItem -Path $env:ChocolateyPackageFolder -Filter *.exe | Remove-Item -Force
 
 $WorkSpace = Join-Path $env:TEMP "$env:ChocolateyPackageName.$env:chocolateyPackageVersion"
 
+# PSExec is a subcomponent of PSTools in which any other subcomponent
+#    could change without any change to PSExec.  This makes tracking
+#    the checksum very difficult, so that aspect is skipped here.
 $WebFileArgs = @{
    packageName    = $env:ChocolateyPackageName
    FileFullPath   = Join-Path $WorkSpace "$env:ChocolateyPackageName.zip"
    Url            = $Url
-   Checksum       = $checkSum
-   ChecksumType   = 'sha256'
    GetOriginalFileName = $true
 }
 $PsToolsZip = Get-ChocolateyWebFile @WebFileArgs
