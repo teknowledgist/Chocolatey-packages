14cafedf9547fd82a27e62ddf381dfa9ad713602
teknowledgist <teknowledgist@users.noreply.github.com>
2022-04-14 12:06:57 -0400

    upgrade tableau-public to 2022.1

--- automatic/tableau-public/tools/chocolateyinstall.ps1
+++ automatic/tableau-public/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    fileType      = 'EXE'
    url64bit      = 'https://public.tableau.com/s/download/public/pc64'
-   checksum64    = 'b43f93f9a3051ca22284b86359bbf8c6b385bdd5053d8fb8bc68a27939eae389'
+   checksum64    = '0bcfb20113a1a285c21080216e7b701cf6480803485a39ebc73ad4e433777238'
    checksumType  = 'sha256'
    silentArgs    = "/quiet /norestart /LOG `"$($env:TEMP)\TableauPublic-$($env:chocolateyPackageVersion)-InstallLogs\Install.log`" ACCEPTEULA=1"
    validExitCodes= @(0,3010)
