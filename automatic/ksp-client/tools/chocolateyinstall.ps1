dce465b3d39e927187b806d558fc8ad5367ea511
teknowledgist <teknowledgist@users.noreply.github.com>
2022-06-16 14:53:29 -0400

    update ksp-client to 7.8.0.8

--- automatic/ksp-client/tools/chocolateyinstall.ps1
+++ automatic/ksp-client/tools/chocolateyinstall.ps1
@@ -24,8 +24,8 @@ $InstallArgs = @{
    fileType      = 'EXE'
    url           = "$DownloadServer/ksp-client-i386$CondensedVersion.exe"
    url64bit      = "$DownloadServer/ksp-client-x64$CondensedVersion.exe"
-   checksum      = '1617c45007ccc5046afef163f25098dcc4a575f5a12fae39324f2b9aca0e514e'
-   checksum64    = '2bfb6b0ce174bcdf4008a8faa3db6d960d92057b2b8b16141d49f4abba45a1d9'
+   checksum      = 'db4fde08dcf9d7cf9b5cc90f3affa91f94bd328e0b930decc7221793ed935d93'
+   checksum64    = 'e4c716d329890bd95c88b3b9b0990e0e9c5c48164a52e27d0b4e50263013dddf'
    checksumType  = 'sha256'
    silentArgs    = "-q -platform $BitLevel -upg $HostSwitch -v PROP_REBOOT=0 -v PROP_SHORTCUTS=0"
    validExitCodes= @(0)
