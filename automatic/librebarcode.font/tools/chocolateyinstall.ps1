148c3f82a8ad8e376e8923015d7077f6d3aca33d
teknowledgist <teknowledgist@users.noreply.github.com>
2022-10-12 13:56:58 -0400

    fix librebarcode.font install problem

--- automatic/librebarcode.font/tools/chocolateyinstall.ps1
+++ automatic/librebarcode.font/tools/chocolateyinstall.ps1
@@ -9,27 +9,27 @@ $UnzipArgs = @{
    FileFullPath   = $fileLocation
    Destination    = $UnzipDir
 }
-Get-ChocolateyUnzip @UnzipArgs
+$null = Get-ChocolateyUnzip @UnzipArgs
 
 
 $AllFontFiles = Get-ChildItem $UnzipDir -Include "*.TTF" -Recurse | 
                   Select-Object -ExpandProperty FullName
 
-if ($pp = Get-PackageParameters) {
-   $TargetFontFiles = @()
-   if ($pp.contains('39')) {
-      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '39[^e]'}
-   }
-   if ($pp.contains('39e')) {
-      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '39e'}
-   }
-   if ($pp.contains('128')) {
-      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '128'}
-   }
-   if ($pp.contains('13')) {
-      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '13'}
-   }
-} else {
+$pp = Get-PackageParameters
+$TargetFontFiles = @()
+if ($pp.contains('39')) {
+   $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '39[^e]'}
+}
+if ($pp.contains('39e')) {
+   $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '39e'}
+}
+if ($pp.contains('128')) {
+   $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '128'}
+}
+if ($pp.contains('13')) {
+   $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '13'}
+}
+if (-not $TargetFontFiles) {
    $TargetFontFiles = $AllFontFiles
 }
 
