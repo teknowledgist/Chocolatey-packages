b1352c9ff4c21d718b8c88c54722ea54353cf3c3
teknowledgist <teknowledgist@users.noreply.github.com>
2022-10-12 12:07:24 -0400

    create librebarcode.font package

---
+++ automatic/librebarcode.font/legal/VERIFICATION.md
@@ -0,0 +1,21 @@
+# VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community in verifying that this package's contents are trustworthy.
+
+## Download
+The zipped font files are downloadable from the GitHub releases page:
+- https://github.com/graphicore/librebarcode/releases
+
+or directly:
+- Version : 1.008
+- URL     : https://github.com/graphicore/librebarcode/releases/download/v1.008/LibreBarcode_v1.008.zip
+- SHA256  : e3b9ed8cc7194788f9c9b194d94000e6de054cfbf202f4e20d22981847510d05
+
+## Verify
+You can use one of the following methods to obtain the checksum:
+1. Use powershell function 'Get-Filehash'
+2. Use chocolatey utility 'checksum.exe'
+
+
+## License
+File 'LICENSE.txt' is obtained from and is included in the zip download:
+- https://github.com/graphicore/librebarcode/blob/master/OFL.txt
---
+++ automatic/librebarcode.font/tools/chocolateyinstall.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.zip').FullName
+$UnzipDir = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+$UnzipArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = $fileLocation
+   Destination    = $UnzipDir
+}
+Get-ChocolateyUnzip @UnzipArgs
+
+
+$AllFontFiles = Get-ChildItem $UnzipDir -Include "*.TTF" -Recurse | 
+                  Select-Object -ExpandProperty FullName
+
+if ($pp = Get-PackageParameters) {
+   $TargetFontFiles = @()
+   if ($pp.contains('39')) {
+      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '39[^e]'}
+   }
+   if ($pp.contains('39e')) {
+      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '39e'}
+   }
+   if ($pp.contains('128')) {
+      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '128'}
+   }
+   if ($pp.contains('13')) {
+      $TargetFontFiles += $AllFontFiles | Where-Object {$_ -match '13'}
+   }
+} else {
+   $TargetFontFiles = $AllFontFiles
+}
+
+$Installed = Add-Font $TargetFontFiles -Multiple
+
+If ($Installed -eq 0) {
+   Throw 'All font installation attempts failed!'
+} elseif ($Installed -lt $TargetFontFiles.count) {
+   Write-Host "$Installed fonts were installed." -ForegroundColor Cyan
+   Write-Warning "$($FontFiles.count - $Installed) submitted font paths failed to install."
+} else {
+   Write-Host "$Installed fonts were installed."
+}
+
+Remove-Item $fileLocation -Force
