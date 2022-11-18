3b34c9f04eec51e4d5adc371015ad454b7fa0871
teknowledgist <teknowledgist@users.noreply.github.com>
2022-11-18 16:58:34 -0500

    Fix nessus-agent checksum

--- automatic/nessus-agent/tools/chocolateyinstall.ps1
+++ automatic/nessus-agent/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
    url           = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/17739/download?i_agree_to_tenable_license_agreement=true'
    url64bit      = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/17738/download?i_agree_to_tenable_license_agreement=true'
    softwareName  = 'Nessus Agent*'
-   checksum      = '62143ab255099f105434cf11bb311d663fce667b55109229f875fa2859e7746f'
-   checksum64    = 'e59a4f3506f77b1ecdde7f92d287fa79368298146ac7487638ceb27056d6e144'
+   checksum      = 'ed7bc037245d07596754658e10719ca6bcec44d74c028f0efa678c5468a6613b'
+   checksum64    = '6ecf9cac910b301fd74a5d22dd5480eb8ed972504ab785aa8799be1334d1b3a5'
    checksumType  = 'sha256' 
    silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($Env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`" "
    validExitCodes= @(0, 3010, 1641)
