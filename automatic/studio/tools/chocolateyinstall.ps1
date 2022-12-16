7f19a7fc81f44cf1386981f53d56417c7f89ef18
teknowledgist <teknowledgist@users.noreply.github.com>
2022-12-16 10:09:35 -0500

    update studio to 2.22.12.1

--- automatic/studio/tools/chocolateyinstall.ps1
+++ automatic/studio/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
    fileType      = 'EXE'
    url           = 'https://s3.amazonaws.com/blstudio/Studio2.0/Studio+2.0_32.exe'
    url64bit      = 'https://s3.amazonaws.com/blstudio/Studio2.0/Studio+2.0.exe'
-   checksum      = '2de5d1035061a5bf4f573ab8cb8bf5053d4423ad2955aaf0491ea0ca473cc679'
-   checksum64    = 'a1a6e5056d1315bb71115d9fbd8f74116896e9267a322d47acfd1bbea5c2d2a9'
+   checksum      = '96b89120f5bd19888cb1e02dadaef638ad31b5865a63169bba1b3aef2f3d6dc2'
+   checksum64    = 'f99d3de1dd6bcdc0e8430258570ac82a7060642a4ba8a58a25d34ca2d944bfcd'
    checksumType  = 'sha256'
    silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).Install.log`""
    validExitCodes= @(0)
