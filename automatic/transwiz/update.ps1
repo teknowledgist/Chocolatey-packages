ff18f8cec35ccd64bcef0a9f365d143bd5b98768
teknowledgist <teknowledgist@users.noreply.github.com>
2022-12-27 16:48:49 -0500

    create transwiz

---
+++ automatic/transwiz/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+
+function global:au_GetLatest {
+   $Release = 'https://www.forensit.com/downloads.html'
+   $download_page = Invoke-WebRequest -Uri $Release
+
+   $main = $download_page.links |
+                  where-object {$_.innertext -match 'transwiz'} | 
+                  select -ExpandProperty innertext -First 1
+   $version = $main.split()[-1]
+
+   return @{ 
+      Version = $version
+      URL = 'https://www.forensit.com/Downloads/Transwiz.msi'
+   }
+}
+
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^\s*Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^\s*Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
