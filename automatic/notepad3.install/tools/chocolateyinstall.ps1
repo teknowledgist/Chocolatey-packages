db710b0b3ff49118357de61dc100f8b799c3d542
teknowledgist <teknowledgist@users.noreply.github.com>
2023-02-19 12:23:19 -0500

    updated notepad3 to 6.23.203.2

--- automatic/notepad3.install/tools/chocolateyinstall.ps1
+++ automatic/notepad3.install/tools/chocolateyinstall.ps1
@@ -3,51 +3,16 @@
 $BitLevel = Get-ProcessorBits
 
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
-
-# This package is automated, but the embedded downloads are sometimes .zip and sometimes .exe
-$ZipFiles = Get-ChildItem $toolsDir -Filter '*.zip'
-If ($ZipFiles) {
-   If ($ZipFiles.count -eq 2) {
-      if ($BitLevel -eq '64') {
-         $ZipFile = $ZipFiles | Where-Object {$_.Name -notmatch 'x(86|32)'} 
-      } else {
-         $ZipFile = $ZipFiles | Where-Object {$_.Name -match 'x(86|32)'}
-      }
-   } else { $ZipFile = $ZipFiles }
-
-   $UnzipDir = Join-Path $env:TEMP "$env:ChocolateyPackageName_$env:ChocolateyPackageVersion"
-
-   Get-ChocolateyUnzip -FileFullPath $ZipFile.FullName -Destination $UnzipDir
-} else {
-   $UnzipDir = $toolsDir
-}
+$Installers = Get-ChildItem $toolsDir -Filter '*.exe'
 
 $InstallArgs = @{
    packageName  = $env:ChocolateyPackageName
-   fileType     = 'EXE' 
    softwareName = "$env:ChocolateyPackageName*"
+   fileType     = 'EXE' 
+   File         = $installers | Where-Object {$_.name -match 'x86'} | Select-Object -ExpandProperty fullname
+   File64       = $installers | Where-Object {$_.name -match 'x64'} | Select-Object -ExpandProperty fullname
    silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
-
-$Installers = Get-ChildItem $UnzipDir -Filter '*.exe'
-
-if ($installers.count -eq 2) {
-   # clearly, .zip not included
-   $Installer64 = $installers | Where-Object {$_.name -notmatch 'x(86|32)'} | Select-Object -ExpandProperty fullname
-   $Installer32 = $installers | Where-Object {$_.name -match 'x(86|32)'} | Select-Object -ExpandProperty fullname
-   $InstallArgs.add('File',$Installer32)
-   $InstallArgs.add('File64',$Installer64)
-} else {
-   # if came zipped, then there should be only one .exe
-   if ($BitLevel -eq '64') {
-      $Installer64 = $installers | Where-Object {$_.name -notmatch 'x(86|32)'} | Select-Object -ExpandProperty fullname
-      $InstallArgs.add('File64',$Installer64)
-   } else {
-      $Installer32 = $installers | Select-Object -ExpandProperty fullname
-      $InstallArgs.add('File',$Installer32)
-   }
-}
-
 Install-ChocolateyInstallPackage @InstallArgs
 
 foreach ($exe in $Installers) {
--- automatic/notepad3.install/update.ps1
+++ automatic/notepad3.install/update.ps1
@@ -9,20 +9,8 @@ function global:au_GetLatest {
                Where-Object {$_.FileName -match 'x86_Setup\.exe'} | 
                Select-Object -First 1 -ExpandProperty DownloadURL
    $URL64 = $Release.Assets | 
-               Where-Object {($_.FileName -match 'Setup\.exe') -and ($_.FileName -notmatch 'x86')} | 
+               Where-Object {$_.FileName -match 'x64_Setup\.exe'} | 
                Select-Object -First 1 -ExpandProperty DownloadURL
-   
-   $DownloadURL = 'https://www.rizonesoft.com/downloads/notepad3/'
-   try { 
-      $download_page = Invoke-WebRequest -Uri $DownloadURL
-      $linkLines = $download_page.rawcontent -split 'tr><' | ? {$_ -match $version.replace('.','\.')} | select -First 2
-      $URL64 = $linkLines[0] -split '"' | ? {$_ -match '^https'} | select -first 1
-      $URL32 = $linkLines[1] -split '"' | ? {$_ -match '^https'} | select -first 1
-   }
-   catch {}
-   if (-not ($url64 -and $URL32)) {
-      Throw 'Notepad3 download URLs not usable'
-   }
 
    return @{ 
             Version  = $Version
@@ -49,8 +37,7 @@ function global:au_SearchReplace {
 if ($MyInvocation.InvocationName -ne '.') { 
    function global:au_BeforeUpdate() { 
       Write-host "Downloading Notepad3 $($Latest.Version) files."
-      $FileBase = "Notepad3_$($Latest.Version)_Setup"
-      Get-RemoteFiles -Purge -FileNameBase $FileBase -FileNameSkip 2
+      Get-RemoteFiles -Purge
    }
 
    update -ChecksumFor none -NoCheckChocoVersion
