94bdd586209493d1c9b18198641d2c1b738462b1
teknowledgist <teknowledgist@users.noreply.github.com>
2023-04-06 12:47:21 -0400

    updated sktimestamp to 1.3.7

--- automatic/sktimestamp/tools/chocolateyinstall.ps1
+++ automatic/sktimestamp/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName= 'sktimestamp'
-$url        = 'https://sourceforge.net/projects/stefanstools/files/SKTimeStamp/SKTimeStamp-1.3.5.msi/download' 
-$url64      = 'https://sourceforge.net/projects/stefanstools/files/SKTimeStamp/SKTimeStamp64-1.3.5.msi/download' 
-$Checksum   = 'c5db0d44548dfa40ea9be5dc0e4163482cd8390d1dafe099d5596fa09eef2f20'
-$Checksum64 = '64db45f107181347ea93173f291d09013f2f19e274ba9e6da1d4f183686985f8'
+$url        = 'https://sourceforge.net/projects/stefanstools/files/SKTimeStamp/SKTimeStamp-1.3.7.msi/download' 
+$url64      = 'https://sourceforge.net/projects/stefanstools/files/SKTimeStamp/SKTimeStamp64-1.3.7.msi/download' 
+$Checksum   = '72443a2ba6186ceb23eb6ef947a64f8a8d8587ec0cb74398f09f43b7f7d19ece'
+$Checksum64 = '80b1c79122d22c8720674171e9db47e10b7ea331450fd6af2aca0391ae18c5c5'
 
 $packageArgs = @{
   packageName   = $packageName
