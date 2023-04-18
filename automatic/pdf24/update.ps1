620776e84bbd8e6376835f7561e1e8469f86deed
teknowledgist <teknowledgist@users.noreply.github.com>
2023-04-18 15:31:42 -0400

    reintegrate 32-bit pdf24

--- automatic/pdf24/update.ps1
+++ automatic/pdf24/update.ps1
@@ -1,36 +1,39 @@
 import-module au
 
-$Release = 'https://en.pdf24.org/pdf-creator-changelog.html'
+$AllVersions = 'https://creator.pdf24.org/listVersions.php'
 
 function global:au_GetLatest {
-   $download_page = Invoke-WebRequest -Uri "$Release"
+   $PageData = Invoke-WebRequest -Uri "$AllVersions" -UseBasicParsing
 
-   $soon = $download_page.AllElements | Where-Object {
-                                          $_.tagname -eq 'div' -and 
-                                          $_.class -eq 'date' -and 
-                                          $_.innertext -match 'coming soon'
-                                       }
-   if ($soon) {$OneOrTwo = 2} else {$OneOrTwo = 1}
-   
-   $text = $download_page.AllElements | 
-      Where-Object {$_.tagname -eq 'h2' -and $_.innertext -match '^version'} |
-      Select-Object -ExpandProperty innertext -First $OneOrTwo
-      
-   $version = $text.split()[-1]
-   
-   $url = "https://download.pdf24.org/pdf24-creator-$version.msi"
+   $latest = $PageData.RawContent -split '<tr>' | ? {$_ -match '<td>'} | select -first 4
 
-   return @{ Version = $version; URL32 = $url }
+   $version = $latest -split '</?td>' | ? {$_ -match '^[0-9.]+'} | select -first 1
+
+   $x64row = $latest | ? {$_ -match 'x64\.msi'}
+   $SHA264x64 = $x64row -split '</?td>' | ? {$_ -match '^[0-9a-f]{64}$'}
+
+   $x86row = $latest | ? {$_ -match 'x86\.msi'}
+   $SHA264x86 = $x86row -split '</?td>' | ? {$_ -match '^[0-9a-f]{64}$'}
+
+   return @{ 
+      Version = $version
+      URL32      = "https://download.pdf24.org/pdf24-creator-$version-x86.msi"
+      Checksum32 = $SHA264x86
+      URL64      = "https://download.pdf24.org/pdf24-creator-$version-x64.msi"
+      Checksum64 = $SHA264x64
+      }
 }
 
 
 function global:au_SearchReplace {
    @{
       "tools\chocolateyInstall.ps1" = @{
-            "(^\s*url\s*=\s*)('.*')"    = "`$1'$($Latest.URL32)'"
-            "(^\s*Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(^\s*Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
       }
    }
 }
 
-update
+update -ChecksumFor none
