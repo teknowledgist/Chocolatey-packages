184636e28a55b686e90208054baf26d4a5ab4aca
teknowledgist <teknowledgist@users.noreply.github.com>
2023-04-30 11:55:01 -0400

    fastcopy changes WIP

--- automatic/fastcopy.install/update.ps1
+++ automatic/fastcopy.install/update.ps1
@@ -6,38 +6,44 @@ function isURIWeb($address) {
 }
 
 function global:au_GetLatest {
-   $RootPage = 'https://fastcopy.jp'
-   $HomePage = Invoke-WebRequest -Uri "$RootPage/en/"
+   $RootPage = Invoke-WebRequest -Uri 'https://fastcopy.jp' -UseBasicParsing
 
-   $Text = $HomePage.allelements |Where-Object {
-                                   ($_.tagname -eq 'th') -and 
-                                   ($_.innertext -match "download v*")
-                                } | Select-Object -First 1 -ExpandProperty innertext
+   $HTML = New-Object -Com "HTMLFile"
+   try {
+      $html.IHTMLDocument2_write($RootPage)    # if MS Office installed
+   } catch {
+       $html.write([Text.Encoding]::Unicode.GetBytes($RootPage))   # No MS Office
+   }
+
+   $Text = $HTML.getElementsByTagName('th') |
+               Where-Object { $_.innertext -match "download v*" } | 
+               Select-Object -First 1 -ExpandProperty innertext
     
    $version = $Text -replace "(?s).*v([0-9.]+).*",'$1'
    
-   $Source = $HomePage.links | 
-                Where-Object {$_.innertext -eq 'Source code'} | Select-Object -ExpandProperty href
+#   $Source = $RootPage.links | 
+#                Where-Object {$_.innertext -eq 'Source code'} | Select-Object -ExpandProperty href
 
-   [array]$links = $HomePage.links | 
-                     Where-Object {($_.innertext -eq 'installer')} |
-                     Select-Object -ExpandProperty href
-   Foreach ($link in $links) {
-      if (-not (isURIWeb $link)) {
-         if (isURIWeb "$RootPage$link") {
-            $URL32 = "$RootPage$link"
-            break
-         } else { Continue }
-      }
-      $DownPage = Invoke-WebRequest -Uri $link -ErrorAction SilentlyContinue
-      if ($DownPage) { break }
-   }
-   if ($DownPage -and (-not $URL32)) {
-      $URL32 = $DownPage.links | 
-               Where-Object {$_.href -match '\.(zip|exe)$'} | 
-               Select-Object -First 1 -ExpandProperty href
-   }
+#   [array]$links = $RootPage.links | 
+#                     Where-Object {($_.innertext -eq 'installer')} |
+#                     Select-Object -ExpandProperty href
+#   Foreach ($link in $links) {
+#      if (-not (isURIWeb $link)) {
+#         if (isURIWeb "$RootPage$link") {
+#            $URL32 = "$RootPage$link"
+#            break
+#         } else { Continue }
+#      }
+#      $DownPage = Invoke-WebRequest -Uri $link -ErrorAction SilentlyContinue
+#      if ($DownPage) { break }
+#   }
+#   if ($DownPage -and (-not $URL32)) {
+#      $URL32 = $DownPage.links | 
+#               Where-Object {$_.href -match '\.(zip|exe)$'} | 
+#               Select-Object -First 1 -ExpandProperty href
+#   }
 
+$URL32 = 'https://github.com/FastCopyLab/FastCopyDist2/raw/main/FastCopy5.0.5_installer.exe'
    return @{ 
             Version   = $version
             SourceURL = "$RootPage$Source"
@@ -52,20 +58,7 @@ function global:au_SearchReplace {
          "(^URL\s+:).*"       = "`${1} $($Latest.URL32)"
          "(^Checksum\s+:).*"  = "`${1} $($Latest.Checksum32)"
       }
-      "$($Latest.PackageName).nuspec" = @{
-         "(<projectSourceUrl>).*(</projectSourceUrl>)" = "`$1$($Latest.SourceURL)`$2"
-      }
    }
 }
 
-# A few things should only be done if the script is run directly (i.e. not "dot sourced")
-#   (It is dot sourced in the meta-package.)
-if ($MyInvocation.InvocationName -ne '.') { 
-   function global:au_BeforeUpdate() { 
-      Write-host "Downloading FastCopy v$($Latest.Version)"
-      Get-RemoteFiles -Purge -NoSuffix
-   }
-
-   update -ChecksumFor none -nocheckchocoversion
-   if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
-}
+update -ChecksumFor 32
