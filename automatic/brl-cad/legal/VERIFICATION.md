5ce23b9022e6d652fcdcc4a1839258d87b4d7858
teknowledgist <teknowledgist@users.noreply.github.com>
2023-07-27 12:01:27 -0400

    update BRL-CAD to 7.36.0

---
+++ automatic/brl-cad/legal/VERIFICATION.md
@@ -0,0 +1,24 @@
+# VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community in verifying that this package's contents are trustworthy.
+
+## Download
+BRL-CAD can be downloaded from:
+- https://github.com/BRL-CAD/brlcad/releases
+
+or directly:
+- Version  : 7.36.0
+- URL      : https://github.com/BRL-CAD/brlcad/releases/download/rel-7-36-0/BRL-CAD_7.36.0_win64.msi
+- Checksum : A5F3A1C5D1E0147C465266DC2500FD83BAAEA0F805C2EE3CE98FA7E785820CC0
+
+## Verify
+You can use one of the following methods to obtain the checksum:
+1. Use powershell function 'Get-Filehash'
+2. Use chocolatey utility 'checksum.exe'
+
+
+## License
+The Copying Statement, LPGL text, and BSD license text come from (respectively):
+
+- https://github.com/BRL-CAD/brlcad/blob/main/COPYING
+- https://github.com/BRL-CAD/brlcad/blob/main/doc/legal/lgpl.txt
+- https://github.com/BRL-CAD/brlcad/blob/main/doc/legal/bsd.txt
--- automatic/brl-cad/update.ps1
+++ automatic/brl-cad/update.ps1
@@ -3,29 +3,24 @@ import-module au
 $FilesPage = 'https://sourceforge.net/projects/brlcad/files/'
 
 function global:au_GetLatest {
-   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-   $download_page = Invoke-WebRequest -Uri $FilesPage
+   $Repo = 'https://github.com/BRL-CAD/brlcad'
+   $Release = Get-LatestReleaseOnGitHub -URL $Repo
 
-   $stub = ($download_page.links |
-                 Where-Object {$_.href -match 'latest'} | 
-                 Select-Object -ExpandProperty title).split(':')[0]
-
-   $Version = $stub.split('/') | ? {$_ -match '^[0-9.]+$'}
-
-   $url = "$FilesPage$stub" -replace ' ','%20' -replace '\.exe$','.msi'
+   $version = $Release.Tag.trim('rel-').replace('-','.')
+   $URL64 = $Release.Assets | Where-Object {$_.FileName -match '\.msi'} | Select-Object -Last 1 -ExpandProperty DownloadURL
 
    return @{ 
-            Version    = $Version
-            URL64      = $url
-           }
+      Version = $version
+      URL64   = $URL64
+   }
 }
 
 function global:au_SearchReplace {
     @{
-      "tools\VERIFICATION.txt" = @{
-         "(^Version\s+:).*"  = "`${1} $($Latest.Version)"
-         "(^URL\s+:).*"      = "`${1} $($Latest.URL64)"
-         "(^Checksum\s+:).*" = "`${1} $($Latest.Checksum64)"
+      "legal\VERIFICATION.md" = @{
+         "(^- Version\s+:).*"  = "`${1} $($Latest.Version)"
+         "(^- URL\s+:).*"      = "`${1} $($Latest.URL64)"
+         "(^- Checksum\s+:).*" = "`${1} $($Latest.Checksum64)"
       }
     }
 }
