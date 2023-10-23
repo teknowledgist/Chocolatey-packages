49cb4367f8a908a9424c87a24e92bafb3dde1f17
teknowledgist <teknowledgist@users.noreply.github.com>
2023-10-23 15:43:15 -0400

    create openpuff package

---
+++ automatic/openpuff/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$FolderOfPackage = Split-Path -Parent $toolsDir
+
+# Remove previous versions
+$Previous = Get-ChildItem $FolderOfPackage -filter "$env:ChocolateyPackageName*" | Where-Object { $_.PSIsContainer }
+if ($Previous) {
+   $Previous | ForEach-Object { Remove-Item $_.FullName -Recurse -Force }
+}
+
+$ZipFile = Get-ChildItem $toolsDir -filter '*.zip' | Select-Object -ExpandProperty fullname
+
+$UnZipArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   FileFullPath   = $ZipFile
+   Destination    = Join-Path $FolderOfPackage "$($env:ChocolateyPackageName)_v$env:ChocolateyPackageVersion"
+}
+Get-ChocolateyUnzip @UnZipArgs
+
+Remove-Item $ZipFile -Force 
+
+$ShortcutArgs = @{
+   ShortcutFilePath = Join-Path $env:ProgramData '\Microsoft\Windows\Start Menu\Programs\OpenPuff.lnk'
+   TargetPath       = (Get-ChildItem $UnZipArgs.Destination -filter *.exe -Recurse).fullname
+}
+Install-ChocolateyShortcut @ShortcutArgs
---
+++ automatic/openpuff/update.ps1
@@ -0,0 +1,38 @@
+import-module au
+
+function global:au_GetLatest {
+   $SiteURL = 'https://embeddedsw.net'
+   $DownPage =  Invoke-WebRequest "$SiteURL/OpenPuff_download.html" -UseBasicParsing
+
+   $stub = $DownPage.Links | ? {$_.href -like '*.zip'} | Select-Object -ExpandProperty href
+
+   $URL = "$SiteURL$($stub.trim('.'))"
+
+   $DownPage.allelements | ? {$_}
+
+   $null = $downpage.RawContent.split('>') | where {$_ -match '^v([0-9.]+) '}
+   $version = $matches[1]
+
+   return @{ 
+      Version = $version
+      URL32   = $URL
+   }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "legal\VERIFICATION.md" = @{
+            "(^- Version:).*" = "`${1} $($Latest.Version)"
+            "(^- URL:).*"     = "`${1} $($Latest.URL32)"
+            "(^- SHA256+:).*" = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading OpenPuff $($Latest.Version) zip file"
+   Get-RemoteFiles -Purge -nosuffix
+}
+
+update -ChecksumFor none
