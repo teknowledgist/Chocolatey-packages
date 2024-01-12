bfa3ecb53db00755af96e7424867f6793ada7f52
teknowledgist <teknowledgist@users.noreply.github.com>
2024-01-12 08:56:47 -0500

    update Tableau-Public to 2023.3.1

--- automatic/tableau-public/tools/chocolateyinstall.ps1
+++ automatic/tableau-public/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    fileType      = 'EXE'
    url64bit      = 'https://public.tableau.com/s/download/public/pc64'
-   checksum64    = '61b94b03abbc283a5b3d5a339664f4a152cac325e3e1ab3e289c606271dd1687'
+   checksum64    = '04f58d261da068d03ec748bfe08d6dbe459e81870278a9d9b7ffda2871c1a99d'
    checksumType  = 'sha256'
    silentArgs    = "/quiet /norestart /LOG `"$($env:TEMP)\TableauPublic-$($env:chocolateyPackageVersion)-InstallLogs\Install.log`" ACCEPTEULA=1"
    validExitCodes= @(0,3010)
