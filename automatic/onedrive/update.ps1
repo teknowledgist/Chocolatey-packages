807a877bb2d287b0593e03c3b31496d74e3cb215
teknowledgist <teknowledgist@users.noreply.github.com>
2024-02-13 13:19:47 -0500

    upgrade onedrive to 24.015.0121.0003
    updater enhanced with EverGreen

--- automatic/onedrive/update.ps1
+++ automatic/onedrive/update.ps1
@@ -1,18 +1,15 @@
 import-module au
 
 function global:au_GetLatest {
-   $Release = 'https://support.microsoft.com/en-us/office/onedrive-release-notes-845dcf18-f921-435e-bf28-4e24b95e5fc0'
-   
-   $download_page = Invoke-WebRequest -Uri "$Release" -UseBasicParsing
-
-   $vstrings = $download_page.RawContent -split ">|<" | 
-                     Where-Object {$_ -match '^Version'} | 
-                     Select-Object -first 1
-   $Version = $vstrings.split()[1]
+   $Meta = Get-EvergreenApp microsoftonedrive | 
+            Where-Object {$_.ring -eq 'production' -and 
+                           $_.architecture -eq 'x86' -and
+                           $_.throttle -eq '100'
+                         }
 
    return @{ 
-      Version = $Version
-      URL32   = "https://oneclient.sfx.ms/Win/Installers/$Version/amd64/OneDriveSetup.exe"
+      Version = $Meta.Version
+      URL32   = $Meta.URI
    }
 }
 
