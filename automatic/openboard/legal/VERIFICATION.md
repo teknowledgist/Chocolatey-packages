d26ee7313f358472c5e09382726eaa340d4a543b
teknowledgist <teknowledgist@users.noreply.github.com>
2024-03-06 09:20:59 -0500

    create openboard package

---
+++ automatic/openboard/legal/VERIFICATION.md
@@ -0,0 +1,24 @@
+# VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community in verifying that this package's contents are trustworthy.
+
+## Download
+OpenBoard can be downloaded from the home site:  
+
+    https://openboard.ch/download.en.html
+
+or from the GitHub release page:
+
+    https://github.com/OpenBoard-org/OpenBoard/releases
+
+Direct download:   
+- Version  : 1.7.0
+- URL      : https://github.com/OpenBoard-org/OpenBoard/releases/download/v1.7.0/OpenBoard_Installer_1.7.0.exe
+- CheckSum : f11f22abd1bbb033c32ba57eb967deefb0646ecf14b5d78758eb13513d25644a
+
+## Verify
+You can use one of the following methods to obtain the checksum:
+1. Use powershell function 'Get-Filehash'
+2. Use chocolatey utility 'checksum.exe'
+
+## License
+File 'LICENSE.txt' is from https://github.com/OpenBoard-org/OpenBoard/blob/master/LICENSE
---
+++ automatic/openboard/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+function global:au_GetLatest {
+   $Repo = 'https://github.com/OpenBoard-org/OpenBoard'
+   $Release = Get-LatestReleaseOnGitHub -URL $Repo
+
+   $version = $Release.Tag.trim('v.')
+   if ($version -match 'rc') {
+      $version = '1.0'
+   }
+   $URL32 = $Release.Assets | Where-Object {$_.FileName -match '\.exe'} | Select-Object -First 1 -ExpandProperty DownloadURL
+
+   return @{ 
+      Version = $version
+      URL32   = $URL32
+   }
+}
+
+function global:au_SearchReplace {
+   @{
+      "legal\VERIFICATION.md" = @{
+            "(^- Version:).*" = "`${1} $($Latest.Version)"
+            "(^- URL:).*"     = "`${1} $($Latest.URL32)"
+            "(^- SHA256+:).*" = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading OpenBoard $($Latest.Version) installer file"
+   Get-RemoteFiles -Purge -nosuffix
+}
+
+update -ChecksumFor none
