56e8dbf762a3b7848019f7a86abb55e6834da243
teknowledgist <teknowledgist@users.noreply.github.com>
2024-03-06 08:10:50 -0500

    create diskgenius package

---
+++ automatic/diskgenius/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   installerType  = 'exe'
+   url            = 'https://www.diskgenius.com/dyna_download/?software=DGEngSetup5511508.exe'
+   Checksum       = '3fe4b4318f3dd58734feacf07998149ac9a1f2580c5775fd502ec20baf006264'
+   ChecksumType   = 'sha256'
+   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
+
---
+++ automatic/diskgenius/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+function global:au_GetLatest {
+   $FreeURL = 'https://www.diskgenius.com/free.php'
+   $Page = Invoke-WebRequest -Uri $FreeURL
+
+   $versionline = $Page.AllElements | 
+                  Where-Object {$_.tagname -eq 'div' -and $_.innertext -match "^version"} | 
+                  Select-Object -first 1 -ExpandProperty innertext
+   $version = $versionline -replace '^Version:\s*([0-9.]+).*','$1'
+
+   $URL32 = "https://www.diskgenius.com/dyna_download/?software=DGEngSetup$($version.replace('.','')).exe"
+
+   return @{ 
+            Version  = $Version
+            URL32    = $URL32
+           }
+}
+
+function global:au_SearchReplace {
+   @{
+      "tools\chocolateyInstall.ps1" = @{
+         "(^   url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+         "(^   Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+      }
+   }
+}
+
+Update-Package -ChecksumFor all
