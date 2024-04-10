0a3bf0053849c162d5522b6a6f80ed211e602566
teknowledgist <teknowledgist@users.noreply.github.com>
2024-04-10 09:07:15 -0400

    update nessus-agent to 10.6.0

--- automatic/nessus-agent/tools/chocolateyinstall.ps1
+++ automatic/nessus-agent/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    fileType      = 'MSI'
-   url           = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/21845/download?i_agree_to_tenable_license_agreement=true'
-   url64bit      = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/21844/download?i_agree_to_tenable_license_agreement=true'
+   url           = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/22608/download?i_agree_to_tenable_license_agreement=true'
+   url64bit      = 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/22607/download?i_agree_to_tenable_license_agreement=true'
    softwareName  = 'Nessus Agent*'
-   checksum      = 'c3b83e4a53a3e24b4b0ce9e83dc6586ed4add3e3097ba81a257c5dbcc059abc6'
-   checksum64    = 'f191702ce94f3e63666afde7f6815654611b56d062c94eccce6118f823407813'
+   checksum      = 'ce7f0f8021928fb084c51ae44fd7a61eceb6efd745bf4c4c712e2325a48e5131'
+   checksum64    = '62ca60031251bebe4da932d5c1e8070487c106fd5dc89fd49a98ce7367bc4a26'
    checksumType  = 'sha256' 
    silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($Env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`" "
    validExitCodes= @(0, 3010, 1641)
