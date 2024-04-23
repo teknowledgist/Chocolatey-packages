efe31bccf3c0daa149e8ec42feb6755d38a54885
teknowledgist <teknowledgist@users.noreply.github.com>
2024-04-23 08:49:17 -0400

    update midori-browser to 11.3.2

--- automatic/midori-browser/tools/chocolateyinstall.ps1
+++ automatic/midori-browser/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $InstallArgs = @{
    packageName    = $env:ChocolateyPackageName
    fileType       = 'EXE'
    softwareName   = "$env:ChocolateyPackageName*"
-   url            = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.1/midori-11.3.1.win32.installer.exe'
-   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.1/midori-11.3.1.win64.installer.exe'
+   url            = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.2/midori-11.3.2.win32.installer.exe'
+   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.2/midori-11.3.2.win64.installer.exe'
    checksumType   = 'sha256'
-   checksum       = '67be714d1210030e576965b420fd907f20feca9dd02b0e03a441fef5ac312dc9'
-   checksum64     = 'f32095c6ba7c999dcec785b6d9d3a481d2483441a2f1ab2d73129cecb1634223'
+   checksum       = '2b321fb3afe0c38a18fe2d9defa20c09877672462b1e5e2837b06792b969fb5d'
+   checksum64     = 'ae8321a6ac8284761b4422e50c83a7faad90d663594cb25af8a349a5462eb351'
    silentArgs     = '/S'
    validExitCodes = @(0)
 }
