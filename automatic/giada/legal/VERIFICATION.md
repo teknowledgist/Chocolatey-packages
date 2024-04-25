8938fbf97d5f2e920b8cbe3e785a8a71065c8a2e
teknowledgist <teknowledgist@users.noreply.github.com>
2024-04-25 10:40:52 -0400

    create giada package

---
+++ automatic/giada/legal/VERIFICATION.md
@@ -0,0 +1,21 @@
+# VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community in verifying that this package's contents are trustworthy.
+
+## Download
+The latest version the the self-contained, zipped files can be downloaded from 
+the official site:
+- https://www.giadamusic.com/index#download
+
+or directly:
+- Version: 1.0.0
+- URL: https://www.giadamusic.com//data/giada-1.0.0-x86_64-windows.zip
+- SHA256: A7E20D60B293273946F1D008F8EEEC2EE15C4A57628E86912064BB3084772F20
+
+## Verify
+You can use one of the following methods to obtain the checksum:
+1. Use powershell function 'Get-Filehash'
+2. Use chocolatey utility 'checksum.exe'
+
+
+## License
+File 'LICENSE.txt' is obtained from https://github.com/monocasual/giada/blob/master/COPYING
---
+++ automatic/giada/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+function global:au_GetLatest {
+   $SiteURL = 'https://www.giadamusic.com/'
+   $DownPage =  Invoke-WebRequest "$SiteURL" -UseBasicParsing
+
+   $stub = $DownPage.Links | ? {$_.href -like '*windows.zip'} | Select-Object -ExpandProperty href
+
+   $URL = "$SiteURL/$stub"
+
+   $version = $stub.split('-')[1]
+
+   return @{ 
+      Version = $version
+      URL32   = $URL
+   }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      'legal\VERIFICATION.md' = @{
+            '(^- Version:).*' = "`${1} $($Latest.Version)"
+            '(^- URL:).*'     = "`${1} $($Latest.URL32)"
+            '(^- SHA256+:).*' = "`${1} $($Latest.Checksum32)"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading Giada $($Latest.Version) zip file"
+   Get-RemoteFiles -Purge -nosuffix
+}
+
+update -ChecksumFor none
