4d043267c6d7e06e4b095401671a3532e5d15677
teknowledgist <teknowledgist@users.noreply.github.com>
2024-05-21 12:14:19 -0400

    fix doublecadXT

--- discontinued/doublecadxt/tools/chocolateyinstall.ps1
+++ discontinued/doublecadxt/tools/chocolateyinstall.ps1
@@ -11,7 +11,7 @@ $SN = 'DCAK-5988-0287-0300-3180'  # This is public: http://download.cnet.com/Dou
 $InstallArgs = @{
    PackageName    = $env:ChocolateyPackageName
    FileType       = 'exe'
-   url            = 'http://downloads.imsidesign.com/ESD/DoubleCAD_XT_v5.exe'
+   url            = 'https://s3.us-west-1.wasabisys.com/imsi.downloads.was/downloads/DoubleCAD_XT_v5.exe'
    Checksum       = '8033bd9b706b2f87159c24c15ed7c432b7aa4cfc43356f12ad7066363e9daee9'
    ChecksumType   = 'SHA256'
    silentArgs     = "/s /a /s /v`"/qn /norestart $MSILog ALLUSERS=1 PIDKEY=$SN`""
