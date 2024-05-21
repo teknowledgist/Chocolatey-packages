73df36aff9f3ea7fb9661286989a55c2ba6a7d6e
teknowledgist <teknowledgist@users.noreply.github.com>
2024-05-21 11:57:38 -0400

    update jana2006 to 2023.02.20

--- manual/jana2006/tools/chocolateyInstall.ps1
+++ manual/jana2006/tools/chocolateyInstall.ps1
@@ -1,10 +1,12 @@
+$ErrorActionPreference = 'Stop'
+
 $installArgs = @{
    packageName = 'jana2006'
    installerType = 'msi'
    url = 'http://www-xray.fzu.cz/jana/download/stable2006/janainst.msi'
-   checkSum = '20DCA8F96855F1EB716418135B8B02D73936C992DA8CC0701B5D4010384036D4'
+   checkSum = '354ebf1c7c1440fc20501b4e39e6305980a108d87a085e1f1f0c15c26902c50f'
    checkSumType = 'sha256'
-   silentArgs = '/qn'
+   silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
    validExitCodes = @(0,3010)
 }
 
