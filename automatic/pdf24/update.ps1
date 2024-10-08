f3047c77e00edf746e26bd174d4a5696ac504441
teknowledgist <teknowledgist@users.noreply.github.com>
2024-10-08 12:59:31 -0400

    (pdf24) update to 11.20.1 and fix update.ps1

--- automatic/pdf24/update.ps1
+++ automatic/pdf24/update.ps1
@@ -5,18 +5,16 @@ $AllVersions = 'https://creator.pdf24.org/listVersions.php'
 function global:au_GetLatest {
    $PageData = Invoke-WebRequest -Uri "$AllVersions" -UseBasicParsing
 
-   $latest = $PageData.RawContent -split '<tr>' | ? {$_ -match '<td>'} | select -first 4
+   $version = ($PageData.rawcontent -split '</?td>')[1]
 
-   $version = $latest -split '</?td>' | ? {$_ -match '^[0-9.]+'} | select -first 1
-
-   $x64row = $latest | ? {$_ -match 'x64\.msi'}
+   $x64row = $pagedata.rawcontent -split '</?tr>' | ? {$_ -match "$version-x64\.msi"}
    $SHA264x64 = $x64row -split '</?td>' | ? {$_ -match '^[0-9a-f]{64}$'}
 
-   $x86row = $latest | ? {$_ -match 'x86\.msi'}
+   $x86row = $pagedata.rawcontent -split '</?tr>' | ? {$_ -match '$version-x86\.msi'}
    $SHA264x86 = $x86row -split '</?td>' | ? {$_ -match '^[0-9a-f]{64}$'}
 
    return @{ 
-      Version = $version
+      Version    = $version
       URL32      = "https://download.pdf24.org/pdf24-creator-$version-x86.msi"
       Checksum32 = $SHA264x86
       URL64      = "https://download.pdf24.org/pdf24-creator-$version-x64.msi"
