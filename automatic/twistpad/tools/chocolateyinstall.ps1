f5bbebc807fe5e90454c95e841e66fca873940fb
teknowledgist <teknowledgist@users.noreply.github.com>
2024-11-30 16:39:27 -0500

    (twistpad) package created - unfinished

---
+++ automatic/twistpad/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   fileType       = 'EXE'
+   softwareName   = "$env:ChocolateyPackageName*"
+   url            = ''
+   checksumType   = 'sha256'
+   checksum       = ''
+   silentArgs     = '/S'
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs 
