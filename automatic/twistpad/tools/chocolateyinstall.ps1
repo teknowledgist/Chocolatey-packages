750c93cb7a279da7a7d3a7b564fbbd33de0d517c
teknowledgist <teknowledgist@users.noreply.github.com>
2024-11-30 20:46:30 -0500

    (twistpad) finish creating package

--- automatic/twistpad/tools/chocolateyinstall.ps1
+++ automatic/twistpad/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $InstallArgs = @{
-   packageName    = $env:ChocolateyPackageName
-   fileType       = 'EXE'
-   softwareName   = "$env:ChocolateyPackageName*"
-   url            = ''
-   checksumType   = 'sha256'
-   checksum       = ''
-   silentArgs     = '/S'
-   validExitCodes = @(0)
+   PackageName    = $env:ChocolateyPackageName
+   FileType       = 'EXE'
+   SoftwareName   = "$env:ChocolateyPackageName*"
+   Url            = 'https://www.carthagosoft.net/downloads/TwistpadSetup.zip'
+   ChecksumType   = 'sha256'
+   Checksum       = '732a30a080168e23489fc0ee14236298583cd9d84018b4c63130d4a4aa35899a'
+   SilentArgs     = '/S'
+   ValidExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @InstallArgs 
---
+++ automatic/twistpad/update.ps1
@@ -0,0 +1,33 @@
+import-module chocolatey-au
+
+function global:au_GetLatest {
+   $DownloadURI = "https://www.carthagosoft.net/TwistpadDownload.php"
+   $PageText = Invoke-WebRequest -Uri $DownloadURI -UseBasicParsing
+
+   $HTML = New-Object -Com "HTMLFile"
+   try {
+      $html.IHTMLDocument2_write($PageText)    # if MS Office installed
+   } catch {
+      $html.write([Text.Encoding]::Unicode.GetBytes($PageText))   # No MS Office
+   }
+
+   $DloadVersion = $html.links | Where-Object {$_.href -match 'downloadfree'} | Select-Object -First 1 -ExpandProperty textcontent
+   $version = ($DloadVersion.split() | Where-Object {$_ -match '^v?[0-9.]+$'}) -replace 'v',''
+
+   return @{ 
+            Version      = $version
+            URL32        = "https://www.carthagosoft.net/downloads/TwistpadSetup.zip"
+           }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "tools\chocolateyInstall.ps1" = @{
+         "(^   url\s*=\s*)('.*')"   = "`$1'$($Latest.URL32)'"
+         "(^   Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      }
+   }
+}
+
+Update-Package -ChecksumFor 32 -NoCheckChocoVersion
