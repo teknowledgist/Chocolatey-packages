59dc1db66e556bda065e519e5bfb441b138dc196
teknowledgist <teknowledgist@users.noreply.github.com>
2024-12-04 11:31:07 -0500

    (traytoolbar) create package

---
+++ automatic/traytoolbar/legal/VERIFICATION.md
@@ -0,0 +1,22 @@
+# VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community in verifying that this package's contents are trustworthy.
+
+## Download
+The latest version the the self-contained, zipped files can be downloaded from 
+the official GitHub page:
+- https://github.com/brondavies/TrayToolbar/releases/latest
+
+or directly:
+- Version: 
+- URL: 
+- SHA256: 
+
+## Verify
+You can use one of the following methods to obtain the checksum:
+1. Use powershell function 'Get-Filehash'
+2. Use chocolatey utility 'checksum.exe'
+
+
+## License
+File 'LICENSE.txt' is obtained from:
+- https://github.com/brondavies/TrayToolbar/blob/master/LICENSE
---
+++ automatic/traytoolbar/update.ps1
@@ -0,0 +1,36 @@
+import-module chocolatey-au
+
+function global:au_GetLatest {
+   $Repo = 'https://github.com/brondavies/TrayToolbar'
+   $Release = Get-LatestReleaseOnGitHub -URL $Repo
+
+   $version = $Release.Tag.trim('v.')
+   $Asset = $Release.Assets | Where-Object {$_.FileName -match 'win-x64.*\.zip$'} | Select-Object -First 1 
+
+   return @{ 
+      Version = $version
+      URL32   = $Asset.DownloadURL
+      ZipFile = $Asset.FileName
+   }
+}
+
+
+function global:au_SearchReplace {
+   @{
+      "legal\VERIFICATION.md" = @{
+            "(^- Version:).*" = "`${1} $($Latest.Version)"
+            "(^- URL:).*"     = "`${1} $($Latest.URL32)"
+            "(^- SHA256+:).*" = "`${1} $($Latest.Checksum32)"
+      }
+      'tools\chocolateyinstall.ps1' = @{
+            '^(\$ZipFile = ).*' = "`${1}'$($Latest.ZipFile)'"
+      }
+   }
+}
+
+function global:au_BeforeUpdate() { 
+   Write-host "Downloading TrayToolbar $($Latest.Version) zip file"
+   Get-RemoteFiles -Purge -nosuffix
+}
+
+update -ChecksumFor none
