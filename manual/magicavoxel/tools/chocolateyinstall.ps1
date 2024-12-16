034875d8af1bf48e7ada8da64b48e0e7423208ed
teknowledgist <teknowledgist@users.noreply.github.com>
2024-12-16 09:54:55 -0500

    (magicavoxel) create package

---
+++ manual/magicavoxel/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$FolderOfPackage = Split-Path -Parent $toolsDir
+
+# Remove previous versions
+$Previous = Get-ChildItem $FolderOfPackage -filter 'MagicaVoxel' -Directory 
+if ($Previous) {
+   $Previous | ForEach-Object { Remove-Item $_.FullName -Recurse -Force }
+}
+
+$ZipArgs = @{
+   packageName   = $env:ChocolateyPackageName
+   unzipLocation = $FolderOfPackage
+   url           = 'https://github.com/ephtracy/ephtracy.github.io/releases/download/0.99.7/MagicaVoxel-0.99.7.1-win64.zip'
+   checksum      = 'cff21ae04622f0b1a64b33c66f13fe5076f3002acabd1d72f4c4939a7df1e1de'
+   checksumType  = 'sha256' 
+}
+
+Install-ChocolateyZipPackage @ZipArgs
+
+$GUI = (Get-ChildItem $ZipArgs.UnzipLocation -filter *.exe -Recurse).fullname
+$null = New-Item "$GUI.gui" -Type file -Force
+
+$StartPrograms = Join-Path $env:ProgramData '\Microsoft\Windows\Start Menu\Programs'
+
+$ShortcutArgs = @{
+   ShortcutFilePath = Join-Path $StartPrograms "MagicaVoxel.lnk"
+   TargetPath = $GUI
+}
+
+Install-ChocolateyShortcut @ShortcutArgs
