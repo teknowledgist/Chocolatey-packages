d166f13eb8f35170bc744cd1e2d4727aded9abc4
teknowledgist <teknowledgist@users.noreply.github.com>
2025-01-13 10:30:30 -0500

    (powerbi-reportbuilder) removed language selection

--- automatic/powerbi-reportbuilder/update.ps1
+++ automatic/powerbi-reportbuilder/update.ps1
@@ -13,51 +13,24 @@ function global:au_GetLatest {
    $CSV = @('Code,Language,URL,SHA256')
    $CSV += "en-EN,English,$url32"
 
-   # Gather other languages download info
-   $mainpage.RawContent -match 'LocaleDropdown":\[(.*?)]'
-   $Languages = $matches[1].trim("}{") -replace '},{',"`r`n" -replace '"cultureCode":"','' -replace '"name":"','' -replace '"',''
-   $Languages = $Languages.split("`r`n") | Where-Object {$_ -and $_ -notmatch 'en-us'}
-   foreach ($lang in $Languages) {
-      Write-Host "Finding URL for:  $Lang"
-      $confirmURL = "https://www.microsoft.com/$($Lang.split(',')[0])/download/details.aspx?id=$ProductID"
-      $confirmpage = Invoke-WebRequest $confirmURL -UseBasicParsing
-      $LangURL = $confirmpage.rawcontent.split('"') | Where-Object {$_ -match '^https.*\.msi$'} | Select-Object -first 1
-      $CSV += "$([System.Net.WebUtility]::HtmlDecode($Lang)),$LangURL"
-   }
-   $CSV | Out-File '.\tools\LanguageChecksums.csv' -Force
-
-   $LatestHT = @{
+   return @{
       Version  = $Version
       URL32    = $URL32
    }
-   foreach ($line in $CSV) {
-      $LangCode = $line.split(',')[0]
-      $LangDL = $line.split(',')[-1]
-      $LatestHT.Add($LangCode,$LangDL)
-   }
-   return $LatestHT
 }
 
 function global:au_SearchReplace {
-   $LangHT = @{}
-   $LangList = @{}
-   $Latest.GetEnumerator().Name | Where-Object {$_ -match '^..-..$' } | ForEach-Object {
-      $LangHT.add("(^$($_),.*?,).*","`$1$($Latest.$_)," + $Latest."$($_)checksum")
-   }
    @{
       'tools\chocolateyInstall.ps1' = @{
          "(^   url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
          "(^   Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       }
-      'tools\LanguageChecksums.csv' = $LangHT
    }
 }
 
 function global:au_BeforeUpdate() {
-   $Latest.GetEnumerator().Name | Where-Object {$_ -match '^..-..$' } | ForEach-Object {
-      Write-Host "Downloading $_ language version."
-      $Latest."$($_)checksum" = Get-RemoteChecksum $Latest.$_
-   }
+   Write-host "Downloading $env:ChocolateyPackageName $($Latest.Version) zip file"
+   Get-RemoteFiles -Purge -NoSuffix
 }
 
 Update-Package -ChecksumFor all
