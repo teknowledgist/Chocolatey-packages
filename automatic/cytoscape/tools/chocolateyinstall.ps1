4c914143780f5caa05a2e29ab16e6a00491ec513
teknowledgist <teknowledgist@users.noreply.github.com>
2025-02-12 08:47:53 -0500

    (cytoscape) create package

---
+++ automatic/cytoscape/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$InstallArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   installerType  = 'exe'
+   Url64bit       = 'https://github.com/cytoscape/cytoscape/releases/download/3.10.3/Cytoscape_3_10_3_windows_64bit.exe'
+   Checksum64     = 'ef53eb6ed290736663d0312c6d149a91155957f15b6a13eb66f4fff2f3d5bd9b'
+   ChecksumType   = 'sha256'
+   silentArgs    = "-q"
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @InstallArgs
---
+++ automatic/cytoscape/update.ps1
@@ -0,0 +1,34 @@
+import-module chocolatey-au
+
+function global:au_GetLatest {
+   $Repo = 'https://github.com/cytoscape/cytoscape'
+   $Release = Get-LatestReleaseOnGitHub -URL $Repo
+
+   $URL64 = $Release.Assets | Where-Object {$_.FileName -match '\.exe'} | Select-Object -First 1 -ExpandProperty DownloadURL
+
+# Sometimes there is no Windows build
+if (-not $url64) { 
+   $version = '1.0'
+   $URL64 = $Release.Assets | Select-Object -First 1 -ExpandProperty DownloadURL
+} else { 
+   $version = $Release.Tag.trim('v.')
+}
+
+
+   return @{ 
+      Version = $version
+      URL64   = $URL64
+   }
+}
+
+
+function global:au_SearchReplace {
+   @{
+       "tools\chocolateyInstall.ps1" = @{
+          "(^\s*Url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+          "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      }
+   }
+}
+
+Update-Package -ChecksumFor 64
