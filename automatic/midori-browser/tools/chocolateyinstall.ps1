2f11db04de5943a0fcd2fd1da86a7e1a08a96d63
Alexander Bejarano <abhieud@yahoo.es>
2025-04-08 22:57:28 +0200

    Update chocolateyinstall.ps1

--- automatic/midori-browser/tools/chocolateyinstall.ps1
+++ automatic/midori-browser/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $InstallArgs = @{
    fileType       = 'EXE'
    softwareName   = "$env:ChocolateyPackageName*"
 #   url            = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.3/midori-11.3.3.win32.installer.exe'
-   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.5/midori-11.5.win64.installer.exe'
+   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.5.2/midori-win64.installer.exe'
    checksumType   = 'sha256'
 #   checksum       = 'be8f251a4cbb3e1c2e84c6b27db832e3df96093dbeed68b7ecab9209fb8da02f'
-   checksum64     = 'b1892cc0d01e0343de81cf8a1cc786d6966046229e0a5a2cdefc21722443824a'
+   checksum64     = 'b8638642e1299db2fc69edb1dbf7e5cdacdf94c17651ed822873982f1001e815'
    silentArgs     = '/S'
    validExitCodes = @(0)
 }
