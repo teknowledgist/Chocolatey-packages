709afc985ca1cd021b8972dc512a67b3ebe8a097
teknowledgist <teknowledgist@users.noreply.github.com>
2025-07-07 10:31:27 -0400

    (openhashtab) convert to .msi

--- automatic/openhashtab/legal/VERIFICATION.md
+++ automatic/openhashtab/legal/VERIFICATION.md
@@ -6,9 +6,11 @@ The installer can be downloaded from the GitHub releases page:
 - https://github.com/namazso/OpenHashTab/releases
 
 or directly:
-Version : 3.0.5
-URL     : https://github.com/namazso/OpenHashTab/releases/download/v3.0.5/OpenHashTab_setup.exe
-SHA256  : 98FB1F2D230DFA0A26915C5B314881FBB9FA76B9EDBB720FDEACA35F3CB35F95
+Version : 3.1.1
+x86_URL     : https://github.com/namazso/OpenHashTab/releases/download/3.1.1/OpenHashTab_Machine_x86.msi
+x86_SHA256  : 51C0C1E9936B77F4EE9AD6A6BEE225FCB0B37ECE38153E1006A5B6EF9F5A7F66
+x64_URL     : https://github.com/namazso/OpenHashTab/releases/download/3.1.1/OpenHashTab_Machine_x64.msi
+x64_SHA256  : abd24e22853404d7468e461521694c6295740b5eef7aabda3dbcd548c09cf67f
 
 ## Verify
 You can use one of the following methods to obtain the checksum:
--- automatic/openhashtab/update.ps1
+++ automatic/openhashtab/update.ps1
@@ -5,19 +5,30 @@ function global:au_GetLatest {
    $Release = Get-LatestReleaseOnGitHub -URL $Repo
 
    $version = $Release.Tag.trim('v.')
-   $URL = $Release.Assets | 
-               Where-Object {$_.FileName -match '\.exe$'} | 
-               Select-Object -First 1 -ExpandProperty DownloadURL
+   $Asset64 = $Release.Assets | 
+               Where-Object {$_.FileName -match 'machine_x64\.msi$'} | 
+               Select-Object -First 1
+   $Asset32 = $Release.Assets | 
+      Where-Object { $_.FileName -match 'machine_x86\.msi$' } | 
+      Select-Object -First 1
 
-   return @{ Version = $version; URL32 = $url }
+   return @{ 
+      Version    = $version
+      URL32      = $Asset32.DownloadURL
+      URL64      = $Asset64.DownloadURL
+      Checksum32 = $Asset32.SHA256
+      Checksum64 = $Asset64.SHA256
+   }
 }
 
 function global:au_SearchReplace {
    @{
       "legal\VERIFICATION.md" = @{
          "(^Version\s+:).*"  = "`${1} $($Latest.Version)"
-         "(^URL\s+:).*"      = "`${1} $($Latest.URL32)"
-         "(^SHA256\s+:).*" = "`${1} $($Latest.Checksum32)"
+         "(^x86_URL\s+:).*"      = "`${1} $($Latest.URL32)"
+         "(^x86_SHA256\s+:).*" = "`${1} $($Latest.Checksum32)"
+         "(^x64_URL\s+:).*"    = "`${1} $($Latest.URL64)"
+         "(^x64_SHA256\s+:).*" = "`${1} $($Latest.Checksum64)"
       }
    }
 }
