7dc5bea44deb12d656fb254174cebf6012c05d41
teknowledgist <teknowledgist@users.noreply.github.com>
2025-08-08 15:15:04 -0400

    (marcedit) update to 7.7.35

---
+++ automatic/marcedit/legal/VERIFICATION.md
@@ -0,0 +1,23 @@
+# VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community in verifying that this package's contents are trustworthy.
+
+## Download
+The latest version of MarcEdit can be downloaded from the official site:
+   https://marcedit.reeset.net/downloads
+and clicking the "Windows Build Options" link and downloading the "Consolidated Build".
+
+Direct download:   
+- Version : 7.7.35
+- URL     : https://marcedit.reeset.net/software/marcedit75/MarcEdit_7_7_mixed.exe
+- SHA256  : 1F05308984BFCDF3A8E4643CFB5EF419AC9157B86C6DE36877D4E65710051E48
+
+## Verify
+You can use one of the following methods to obtain the checksum:
+1. Use powershell function 'Get-Filehash'
+2. Use chocolatey utility 'checksum.exe'
+
+## License
+File 'LICENSE.txt' is from:  https://marcedit.reeset.net/marcedit-end-user-license-agreement
+
+
+
--- automatic/marcedit/tools/chocolateyinstall.ps1
+++ automatic/marcedit/tools/chocolateyinstall.ps1
@@ -1,16 +1,44 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$Installers = Get-ChildItem $toolsDir -filter '*.msi'
+# Remove possible previous install
+Write-Warning 'Checking for previous install.'
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'marcedit*'
+$SameID = $false
+if ($key.Count -le 2) {
+   if ($key.Count -eq 2) {
+      $First = $key[0].UninstallString -replace '.*\{(.*)}.*','$1'
+      $Second = $key[1].UninstallString -replace '.*\{(.*)}.*', '$1'
+      if ($first -eq $second) {
+         $SameID = $true
+      }
+   }
+   If ($key.Count -eq 1 -or $SameID) {
+      $UninstallArgs = ($key[0].UninstallString -replace '.*msiexec\.exe /i','/x') + " /qn /norestart"
+      $RemoveProc = Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList $UninstallArgs -PassThru
+      $updateId = $RemoveProc.Id
+      Write-Debug 'Uninstalling old version of marcedit.'
+      Write-Debug "Uninstall Process ID:`t$updateId"
+      $RemoveProc.WaitForExit()
+   } 
+}
+if (($key.Count -ge 2) -and (-not $SameID)) {
+   Write-Warning 'Multiple, previous installs found!  This package will attempt to install the latest version, but no previous installs will be removed.'
+}
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$File = Get-ChildItem $toolsDir -Filter '*.exe' |
+         Sort-Object LastWriteTime | 
+         Select-Object -Last 1 -ExpandProperty fullname
 
 $packageArgs = @{
-   packageName   = $env:chocolateyPackageName
-   fileType      = 'msi'
-   File           = ($Installers | Where-Object {$_.basename -match '32'}).FullName
-   File64         = ($Installers | Where-Object {$_.basename -match '64'}).FullName
-   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+   packageName    = $env:ChocolateyPackageName
+   fileType       = 'EXE'
+   File           = $File
+   softwareName   = "$env:ChocolateyPackageName*"
+   silentArgs    = " /exenoui /noprereqs /q NOAUTOUPDATE=1"
    validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs 
 
+Remove-Item $File -ea 0 -force
