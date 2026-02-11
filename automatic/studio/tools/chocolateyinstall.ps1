359f9cc477b30e8502bbeb1b9749fc52096f8f77
teknowledgist <teknowledgist@users.noreply.github.com>
2026-02-11 11:41:52 -0500

    (studio) update to 2.26.1.1

--- automatic/studio/tools/chocolateyinstall.ps1
+++ automatic/studio/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $packageArgs = @{
    packageName   = $env:ChocolateyPackageName
    softwareName  = 'Studio 2.0*' 
    fileType      = 'EXE'
-   url           = 'https://studio.download.bricklink.info/Studio2.0/Archive/2.25.11_3/Studio+2.0_32.exe'
-   url64bit      = 'https://studio.download.bricklink.info/Studio2.0/Archive/2.25.11_3/Studio+2.0.exe'
-   checksum      = 'd2f73d0aef2acfb835c6d385397d00dd236cbbd91779ca012c221bccfc60d4be'
-   checksum64    = 'a3d3b9942bffcb5c28529c6ee13ba31d6a4302358becc314f5a80e5318bd8b08'
+   url           = 'https://studio.download.bricklink.info/Studio2.0/Archive/2.26.1_1/Studio+2.0_32.exe'
+   url64bit      = 'https://studio.download.bricklink.info/Studio2.0/Archive/2.26.1_1/Studio+2.0.exe'
+   checksum      = 'b8cd03ee7aff74fba043cdf7cec25ac50f893aa686522410791ae70028aa6d40'
+   checksum64    = 'e87351bbacd45f47b4fd254507010ed796d61ccf560cf70c55a28799b06875ee'
    checksumType  = 'sha256'
    silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).Install.log`""
    validExitCodes= @(0)
