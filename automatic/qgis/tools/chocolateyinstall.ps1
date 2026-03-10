1f0e255770dbef246bf19034682ad429c0465f01
teknowledgist <teknowledgist@users.noreply.github.com>
2026-03-10 10:39:38 -0400

    (qgis) update to 4.0.0

--- automatic/qgis/tools/chocolateyinstall.ps1
+++ automatic/qgis/tools/chocolateyinstall.ps1
@@ -4,15 +4,15 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $FolderOfPackage = Split-Path -Parent $toolsDir
 
 $NewRelease = $env:ChocolateyPackageVersion
-$LTRversion = '3.40.15'
+$LTRversion = '3.44.8'
 
 $InstallArgs = @{
    packageName    = 'qgis'
    fileType       = 'MSI'
    softwareName   = "$env:ChocolateyPackageName $env:ChocolateyPackageVersion*"
-   url64bit       = 'https://download.osgeo.org/qgis/windows/QGIS-OSGeo4W-3.44.7-1.msi'
+   url64bit       = 'https://download.osgeo.org/qgis/windows/QGIS-OSGeo4W-4.0.0-1.msi'
    checksumType   = 'sha256'
-   checksum64     = '55aace0e1b416f0360002f38681ab0cb92ad9f1f0fba27b31e6100cfd99d59d3'
+   checksum64     = '283e03a021888cb59cf547f7420638885e66d2764ffb1741417d2ab8a5654c9b'
    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
    validExitCodes = @(0)
 }
