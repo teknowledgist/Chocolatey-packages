e3ac9acf0c856f1b4613ed4547c1bb077d8ce232
teknowledgist <teknowledgist@users.noreply.github.com>
2026-03-31 12:02:05 -0400

    (megasync) update to 6.2.1.0

--- automatic/megasync/tools/chocolateyinstall.ps1
+++ automatic/megasync/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
 #   url            = 'https://mega.nz/MEGAsyncSetup32.exe'
    url64          = 'https://mega.nz/MEGAsyncSetup64.exe'
 #   checksum       = '963a82b9b767f77dfa9edf9864590a0eb813ddb677b72705717ca79b18b8b6a7'
-   checksum64     = 'ae5a6ab91533b900c04517249feee1a8e3dc268e96812258dbd51cb80953843a'
+   checksum64     = '477727feb43dda716a40a280c6e962223fcd92d7f85b9b1b276d24e3112b7d3b'
    checksumType   = 'sha256'
    silentArgs     = '/S /MULTIUSER=true'
    validExitCodes = @(0)
