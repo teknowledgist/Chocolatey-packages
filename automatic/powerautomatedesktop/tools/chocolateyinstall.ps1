5197584eab6572cd485f5ff5653be3570a5288d6
teknowledgist <teknowledgist@users.noreply.github.com>
2026-04-06 14:27:35 -0400

    (powerautomatedesktop) update to 2.66.154.26079

--- automatic/powerautomatedesktop/tools/chocolateyinstall.ps1
+++ automatic/powerautomatedesktop/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $InstallArgs = @{
    packageName    = $env:ChocolateyPackageName
    installerType  = 'exe'
    url            = 'https://go.microsoft.com/fwlink/?linkid=2102613'
-   Checksum       = 'd83bee6d8dab8d024d7a58d634942da98f0f758816ead2b4c9885fc1229dded5'
+   Checksum       = '5d3c1e96fa4a4583bfd8c9235df04325ba1ba155be2910322a520579d534654a'
    ChecksumType   = 'sha256'
    silentArgs    = "-Install -ACCEPTEULA -DISABLETURNONRDP -Silent"
    validExitCodes = @(0)
