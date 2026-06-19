f21e3c03e3698c589c8ac7567bdbfc2eb7bd9e66
Alexander Bejarano <abhieud@yahoo.es>
2026-06-20 00:32:43 +0200

    Update Midori installer URL and checksum

--- automatic/midori-browser/tools/chocolateyinstall.ps1
+++ automatic/midori-browser/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $InstallArgs = @{
    fileType       = 'EXE'
    softwareName   = "$env:ChocolateyPackageName*"
 #   url            = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.3/midori-11.3.3.win32.installer.exe'
-   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.8/midori-11.8.win64.installer.exe'
+   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.8.1/midori-11.8.1.win64.installer.exe'
    checksumType   = 'sha256'
 #   checksum       = 'be8f251a4cbb3e1c2e84c6b27db832e3df96093dbeed68b7ecab9209fb8da02f'
-   checksum64     = 'd8ac4213f094ebd432aea54deeea86630271d7b02d86edae013457a360d4e8b3'
+   checksum64     = '7b5e637ef896078bf71842a331d19493526ed5ac4b35f24f430b750ea7c81330'
    silentArgs     = '/S'
    validExitCodes = @(0)
 }
