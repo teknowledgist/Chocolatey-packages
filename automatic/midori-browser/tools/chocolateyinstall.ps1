54cfcb7a67d10152e351e6785576087e5dfb5779
teknowledgist <teknowledgist@users.noreply.github.com>
2026-07-16 12:59:18 -0400

    (midori-browser) update to v11.9

--- automatic/midori-browser/tools/chocolateyinstall.ps1
+++ automatic/midori-browser/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $InstallArgs = @{
    fileType       = 'EXE'
    softwareName   = "$env:ChocolateyPackageName*"
 #   url            = 'https://github.com/goastian/midori-desktop/releases/download/v11.3.3/midori-11.3.3.win32.installer.exe'
-   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.8.2/midori-11.8.2.win64.installer.exe'
+   url64bit       = 'https://github.com/goastian/midori-desktop/releases/download/v11.9/midori-11.9.win64.installer.exe'
    checksumType   = 'sha256'
 #   checksum       = 'be8f251a4cbb3e1c2e84c6b27db832e3df96093dbeed68b7ecab9209fb8da02f'
-   checksum64     = '455d6dc64c404ecd8af1d37f5f10a7bc480e4413df65b626ed346dc323610984'
+   checksum64     = 'd4e1efff5f9b6c5c216cb381900b853a3333307345e8f367e2e700ba53a0e598'
    silentArgs     = '/S'
    validExitCodes = @(0)
 }
